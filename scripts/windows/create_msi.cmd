078c5514c9600adba6d58a9fb1aa7f2e6849a9be
Ted Chambers <tedchamb@microsoft.com>
2017-10-25 18:35:49 -0400

    pass through args

--- scripts/windows/create_msi.cmd
+++ scripts/windows/create_msi.cmd
@@ -1,4 +1,4 @@
 @REM init section. Set _echo=1 to echo everything
 @IF NOT DEFINED _echo ECHO OFF
 
-%~dp0\..\..\packaged_releases\windows\scripts\build_local.cmd
+%~dp0\..\..\packaged_releases\windows\scripts\build_local.cmd %*
