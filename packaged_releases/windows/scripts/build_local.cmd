baef19a7e74f90ecaa2f4d663c32970c7a25fa6f
Ted Chambers <tedchamb@microsoft.com>
2017-10-25 14:46:35 -0400

    packaging tweaks

--- packaged_releases/windows/scripts/build_local.cmd
+++ packaged_releases/windows/scripts/build_local.cmd
@@ -17,9 +17,9 @@ set WIX_DOWNLOAD_URL="https://azurecliprod.blob.core.windows.net/msi/wix310-bina
 :: Set up the output directory and temp. directories
 echo *** Cleaning previous build artifacts...
 set OUTPUT_DIR=%~dp0..\out
-if exist %OUTPUT_DIR% rmdir /s /q %OUTPUT_DIR%
+if exist "%OUTPUT_DIR%" rmdir /s /q "%OUTPUT_DIR%"
 if %errorlevel% neq 0 goto ERROR
-mkdir %OUTPUT_DIR%
+mkdir "%OUTPUT_DIR%"
 if %errorlevel% neq 0 goto ERROR
 
 set TEMP_SCRATCH_FOLDER=%HOMEDRIVE%%HOMEPATH%\vstscli_scratch
@@ -40,7 +40,7 @@ FOR /f %%i IN ('where python') DO (
   set PY_FILE_NAME=%%~ni
   set PYTHON_EXE=!PY_FILE_DRIVE!!PY_FILE_PATH!!PY_FILE_NAME!.exe
   set PYTHON_DIR=!PY_FILE_DRIVE!!PY_FILE_PATH!
-  FOR /F "delims=" %%j IN ('!PYTHON_EXE! --version') DO (
+  FOR /F "delims=" %%j IN ('"!PYTHON_EXE!" --version') DO (
     set PYTHON_VER=%%j
     echo.!PYTHON_VER!|findstr /C:"%PYTHON_VERSION%" >nul 2>&1
     if not errorlevel 1 (
@@ -56,31 +56,31 @@ exit /b 1
 echo *** Python Executables: %PYTHON_DIR%, %PYTHON_EXE%
 
 ::reset working folders
-if exist %BUILDING_DIR% rmdir /s /q %BUILDING_DIR%
+if exist "%BUILDING_DIR%" rmdir /s /q "%BUILDING_DIR%"
 ::rmdir always returns 0, so check folder's existence 
-if exist %BUILDING_DIR% (
+if exist "%BUILDING_DIR%" (
     echo Failed to delete %BUILDING_DIR%.
     goto ERROR
 )
 mkdir %BUILDING_DIR%
 if %errorlevel% neq 0 goto ERROR
 
-if exist %TEMP_SCRATCH_FOLDER% rmdir /s /q %TEMP_SCRATCH_FOLDER%
-if exist %TEMP_SCRATCH_FOLDER% (
+if exist "%TEMP_SCRATCH_FOLDER%" rmdir /s /q "%TEMP_SCRATCH_FOLDER%"
+if exist "%TEMP_SCRATCH_FOLDER%" (
     echo Failed to delete %TEMP_SCRATCH_FOLDER%.
     goto ERROR
 )
-mkdir %TEMP_SCRATCH_FOLDER%
+mkdir "%TEMP_SCRATCH_FOLDER%"
 if %errorlevel% neq 0 goto ERROR
 
 ::ensure wix is available
-if exist %WIX_DIR% (
+if exist "%WIX_DIR%" (
     echo *** Using existing Wix at %WIX_DIR%
 )
-if not exist %WIX_DIR% (
-    mkdir %WIX_DIR%
+if not exist "%WIX_DIR%" (
+    mkdir "%WIX_DIR%"
     if %errorlevel% neq 0 goto ERROR
-    pushd %WIX_DIR%
+    pushd "%WIX_DIR%"
     echo *** Downloading Wix.
     curl -o wix-archive.zip %WIX_DOWNLOAD_URL% -k
     if %errorlevel% neq 0 goto ERROR
@@ -93,35 +93,35 @@ if not exist %WIX_DIR% (
 )
 
 :: Use the Python version on the machine that creates the MSI
-robocopy %PYTHON_DIR% %BUILDING_DIR% /s /NFL /NDL
+robocopy %PYTHON_DIR% "%BUILDING_DIR%" /s /NFL /NDL
 if %errorlevel% gtr 1 goto ERROR
 
 :: Build & install all the packages with bdist_wheel
-%BUILDING_DIR%\python %~dp0build-packages.py %TEMP_SCRATCH_FOLDER% %REPO_ROOT%
+"%BUILDING_DIR%\python" "%~dp0build-packages.py" "%TEMP_SCRATCH_FOLDER%" "%REPO_ROOT%"
 if %errorlevel% neq 0 goto ERROR
 
 :: Install them to the temp folder so to be packaged
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir vsts --upgrade --no-cache-dir --extra-index-url https://vstscli.azurewebsites.net
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir vsts --upgrade --no-cache-dir --extra-index-url https://vstscli.azurewebsites.net
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-common"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-common"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-build-common"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-build-common"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-code-common"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-code-common"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-team-common"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-team-common"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-work-common"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\common_modules\vsts-cli-work-common"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-build"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-build"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-code"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-code"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-team"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-team"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-work"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\command_modules\vsts-cli-work"
 if %errorlevel% neq 0 goto ERROR
-%BUILDING_DIR%\python.exe -m pip install -f %TEMP_SCRATCH_FOLDER% --no-cache-dir "%REPO_ROOT%\src\vsts-cli"
+%BUILDING_DIR%\python.exe -m pip install -f "%TEMP_SCRATCH_FOLDER%" --no-cache-dir "%REPO_ROOT%\src\vsts-cli"
 if %errorlevel% neq 0 goto ERROR
 %BUILDING_DIR%\python.exe -m pip install --force-reinstall --upgrade knack keyring msrest
 if %errorlevel% neq 0 goto ERROR
@@ -129,11 +129,11 @@ if %errorlevel% neq 0 goto ERROR
 echo *** Creating the wbin (Windows binaries) folder that will be added to the path...
 mkdir %BUILDING_DIR%\wbin
 if %errorlevel% neq 0 goto ERROR
-copy %REPO_ROOT%\packaged_releases\windows\scripts\vsts.cmd %BUILDING_DIR%\wbin\
+copy %REPO_ROOT%\packaged_releases\windows\scripts\vsts.cmd "%BUILDING_DIR%\wbin\"
 if %errorlevel% gtr 1 goto ERROR
-copy %REPO_ROOT%\packaged_releases\windows\resources\CLI_LICENSE.rtf %BUILDING_DIR%
+copy %REPO_ROOT%\packaged_releases\windows\resources\CLI_LICENSE.rtf "%BUILDING_DIR%"
 if %errorlevel% gtr 1 goto ERROR
-copy %REPO_ROOT%\packaged_releases\windows\resources\ThirdPartyNotices.txt %BUILDING_DIR%
+copy %REPO_ROOT%\packaged_releases\windows\resources\ThirdPartyNotices.txt "%BUILDING_DIR%"
 if %errorlevel% gtr 1 goto ERROR
 del %BUILDING_DIR%\Scripts\pip.exe
 if %errorlevel% gtr 1 goto ERROR
