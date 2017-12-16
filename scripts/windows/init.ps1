f8a3475be867a0b6a3c97ce456f3af53812d7c4e
Aleksandar Nikolić <alexandair@live.com>
2017-12-16 23:27:32 +0100

    Fix capitalization.
    Fix capitalization of PowerShell and the cmdlet names.

--- scripts/windows/init.ps1
+++ scripts/windows/init.ps1
@@ -5,14 +5,14 @@ param (
 $tempFile = [IO.Path]::GetTempFileName()
 cmd.exe /C "$PSScriptRoot\init.cmd && set>$tempFile"
 $lines = [System.IO.File]::ReadAllLines("$tempFile")
-$curLoc = get-location
-$lines|ForEach-Object -Begin { set-location env: } -End { set-location $curLoc } -Process {
+$curLoc = Get-Location
+$lines | ForEach-Object -Begin { Set-Location env: } -End { Set-Location $curLoc } -Process {
     $var = $_.Split('=')
     if ($var.length -gt 1 -and $var[0] -ne "") {
-        set-item -path $var[0] -value $var[1]
+        Set-Item -path $var[0] -value $var[1]
     }
 }
-remove-item $tempFile
+Remove-Item $tempFile
 
 # Set up aliases
 
@@ -24,10 +24,10 @@ if ((Get-Module PSReadLine) -and ($KeepPsReadLine -eq $false)) {
     Remove-Module PSReadLine 
 
     # note: using doskey because Set-Alias is not as powerful
-    # NOTE commands in macros.txt work in both Powershell and cmd. Keep it that way!
-    # Only add macros to macros.ps.txt when the same macro cannot be used in both Powershell and cmd.
+    # NOTE commands in macros.txt work in both PowerShell and cmd. Keep it that way!
+    # Only add macros to macros.ps.txt when the same macro cannot be used in both PowerShell and cmd.
     # In that case, add equivalent macros to both macros.ps.txt and macros.cmd.txt, to ensure that
-    # the Powershell and cmd development environments remain functionally identical.
+    # the PowerShell and cmd development environments remain functionally identical.
     doskey /exename=powershell.exe /MACROFILE="$PSScriptRoot\macros.txt"
     doskey /exename=powershell.exe /MACROFILE="$PSScriptRoot\macros.ps.txt"
 }
