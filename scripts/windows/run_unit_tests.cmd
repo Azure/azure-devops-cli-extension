6454624c9d081c28827710b20e51768d51d8cc7b
Ted Chambers <tedchamb@microsoft.com>
2018-07-12 17:34:07 -0400

    another test

--- scripts/windows/run_unit_tests.cmd
+++ scripts/windows/run_unit_tests.cmd
@@ -13,6 +13,9 @@ pushd ""%~dp0\..\..\src\common_modules"
 IF ERRORLEVEL 1 GOTO FAIL
 "%PYTHONEXE%" -m unittest discover -s vsts-cli-team-common -v
 IF ERRORLEVEL 1 GOTO FAIL
+pushd ""%~dp0\..\..\src"
+"%PYTHONEXE%" -m unittest discover -s vsts-cli -v
+IF ERRORLEVEL 1 GOTO FAIL
 popd
 
 
