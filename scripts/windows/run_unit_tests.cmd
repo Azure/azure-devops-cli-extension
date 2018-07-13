1ad1ad0da77d25b9926b2236ae64e2f9034f5473
Ted Chambers <tedchamb@microsoft.com>
2018-07-13 12:11:58 -0400

    fix test script and add missing __init__.py file

--- scripts/windows/run_unit_tests.cmd
+++ scripts/windows/run_unit_tests.cmd
@@ -13,6 +13,7 @@ pushd ""%~dp0\..\..\src\common_modules"
 IF ERRORLEVEL 1 GOTO FAIL
 "%PYTHONEXE%" -m unittest discover -s vsts-cli-team-common -v
 IF ERRORLEVEL 1 GOTO FAIL
+popd
 pushd ""%~dp0\..\..\src"
 "%PYTHONEXE%" -m unittest discover -s vsts-cli -v
 IF ERRORLEVEL 1 GOTO FAIL
