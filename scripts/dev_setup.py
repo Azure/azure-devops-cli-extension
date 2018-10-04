76ddc8a5e819afe7b7a001240ba5ee452080e003
Ishita Mehta <ishitam@microsoft.com>
2018-10-04 06:00:05 +0000

    Merged PR 16: Adding dev setup file

--- scripts/dev_setup.py
+++ scripts/dev_setup.py
@@ -3,18 +3,14 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from __future__ import print_function
-
 import sys
 import os
-from glob import glob
 from subprocess import check_call, CalledProcessError
 
-print('Running dev setup...')
 root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..'))
-print('Root directory \'{}\'\n'.format(root_dir))
-os.chdir(root_dir)
 
+print('Running dev setup...')
+print('Root directory \'{}\'\n'.format(root_dir))
 
 def exec_command(command):
     try:
@@ -25,27 +21,11 @@ def exec_command(command):
         print(err, file=sys.stderr)
         sys.exit(1)
 
-
+# install general requirements and azure-cli
 exec_command('python -m pip install --upgrade pip')
-exec_command('python -m pip install --upgrade wheel')
 
-packages = []
-
-# VSTS Python SDK package (from either local directory or latest)
-if 'vsts-python-api-repo' in os.environ and os.path.isdir(os.environ['vsts-python-api-repo']):
-    packages.append(os.environ['vsts-python-api-repo'] + "/vsts")
-else:
-    exec_command("pip install vsts --upgrade --no-cache-dir --extra-index-url https://vstscli.azurewebsites.net")
-
-# VSTS CLI packages (from local directory)
-packages.append("src/common_modules/vsts-cli-common")
-packages.extend(os.path.dirname(p) for p in glob('src/common_modules/vsts-cli*/setup.py') if 'vsts-cli-common' not in p)
-packages.extend(os.path.dirname(p) for p in glob('src/command_modules/vsts-cli*/setup.py'))
-packages.append("src/vsts-cli")
-
-# install general requirements
 if os.path.isfile('./requirements.txt'):
     exec_command('pip install -r requirements.txt')
 
-# install packages
-exec_command('pip install -e {}'.format(' -e '.join(packages)))
+# upgrade to latest az-dev-cli
+exec_command(r'pip install --upgrade .\azure-dev-cli-extensions')
