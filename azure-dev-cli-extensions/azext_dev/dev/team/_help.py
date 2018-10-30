1954220ba89d5402d9fe2365c53cd4e0e77d285a
GaurÄv Saral e̲ <gsaral@microsoft.com>
2018-10-30 06:31:21 +0000

    Merged PR 62: help for dev
    help for dev

--- azure-dev-cli-extensions/azext_dev/dev/team/_help.py
+++ azure-dev-cli-extensions/azext_dev/dev/team/_help.py
@@ -8,6 +8,12 @@ from knack.help_files import helps  # pylint: disable=unused-import
 # pylint: disable=line-too-long
 
 def load_team_help():
+    helps['dev'] = """
+    type: group
+    short-summary: Commands to work with Azure DevOps Organization level operations.
+    long-summary:
+    """
+
     helps['dev project'] = """
     type: group
     short-summary: Commands to work with and manage team projects.
