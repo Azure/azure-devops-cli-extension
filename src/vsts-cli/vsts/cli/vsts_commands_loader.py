aff689796b8c31775afc3f14387c7891152e8030
Nishu Bansal <nishu.bansal91@gmail.com>
2018-11-13 15:41:30 +0530

    VSTS CLI Release cmds added (#234)
    * vsts release cmds added
    * Release help file updated
    * .gitignore file updated
    * Table output format updated

--- src/vsts-cli/vsts/cli/vsts_commands_loader.py
+++ src/vsts-cli/vsts/cli/vsts_commands_loader.py
@@ -8,6 +8,8 @@ from vsts.cli.admin.commands import load_admin_commands
 from vsts.cli.admin.arguments import load_admin_arguments
 from vsts.cli.build.commands import load_build_commands
 from vsts.cli.build.arguments import load_build_arguments
+from vsts.cli.release.commands import load_release_commands
+from vsts.cli.release.arguments import load_release_arguments
 from vsts.cli.code.commands import load_code_commands
 from vsts.cli.code.arguments import load_code_arguments
 from vsts.cli.team.commands import load_team_commands
@@ -22,6 +24,7 @@ class VstsCommandsLoader(CLICommandsLoader):
     def load_command_table(self, args):
         load_admin_commands(self)
         load_build_commands(self)
+        load_release_commands(self)
         load_code_commands(self)
         load_team_commands(self)
         load_work_commands(self)
@@ -31,6 +34,7 @@ class VstsCommandsLoader(CLICommandsLoader):
     def load_arguments(self, command):
         load_admin_arguments(self)
         load_build_arguments(self)
+        load_release_arguments(self)
         load_code_arguments(self)
         load_team_arguments(self)
         load_work_arguments(self)
