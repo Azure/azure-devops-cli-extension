6d6f11cb5f1a3ff48929808a11bc8ea3aa237a51
Gaurav Saral <gsaral@microsoft.com>
2019-01-07 10:11:12 +0530

    fake commit to check tests failing

--- azure-devops/azext_devops/test/repos/test_pull_request.py
+++ azure-devops/azext_devops/test/repos/test_pull_request.py
@@ -40,7 +40,7 @@ from azext_devops.dev.common.services import clear_connection_cache
 class TestPullRequestMethods(unittest.TestCase):
 
     _TEST_DEVOPS_ORGANIZATION = 'https://azuredevopsclitest.visualstudio.com'
-    _TEST_PAT_TOKEN = 'lwghjbj67fghokrgxsytghg75nk2ssguljk7a78qpcg2ttygviyt'
+    _TEST_PAT_TOKEN = 'lwghjbj67fghokrgxsytghg75nk2ssguljk7a78qpcg2ttygviytxx'
     _TEST_PROJECT_NAME = 'sample_project'
     _TEST_REPOSITORY_NAME = 'sample_repository'
     _TEST_SOURCE_BRANCH = 'sample_source_branch'
