866352e89169b5327f4e280af64143977ba6a91b
Zach Renner <zarenner@microsoft.com>
2019-01-09 12:02:34 -0800

    ArtifactTool: Send distro when determining artifactool to download

--- azure-devops/azext_devops/dev/artifacts/client_tool/client_tool_client.py
+++ azure-devops/azext_devops/dev/artifacts/client_tool/client_tool_client.py
@@ -18,7 +18,15 @@ class ClientToolClient(VssClient):
 
     resource_area_identifier = '3fda18ba-dff2-42e6-8d10-c521b23b85fc'
 
-    def get_clienttool_release(self, tool_name, os_name=None, arch=None, version=None):
+    def get_clienttool_release(
+            self,
+            tool_name,
+            os_name=None,
+            arch=None,
+            distro_name=None,
+            distro_version=None,
+            version=None):
+
         route_values = {}
         if tool_name is not None:
             route_values['toolName'] = self._serialize.url('tool_name', tool_name, 'str')
@@ -27,6 +35,10 @@ class ClientToolClient(VssClient):
             query_parameters['osName'] = self._serialize.query('os_name', os_name, 'str')
         if arch is not None:
             query_parameters['arch'] = self._serialize.query('arch', arch, 'str')
+        if distro_name is not None:
+            query_parameters['distroName'] = self._serialize.query('distro_name', distro_name, 'str')
+        if distro_version is not None:
+            query_parameters['distroVersion'] = self._serialize.query('distro_version', distro_version, 'str')
         if version is not None:
             query_parameters['version'] = self._serialize.query('version', version, 'str')
         response = self._send(http_method='GET',
