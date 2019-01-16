1c83eaf00551e937df19d9a16ea305faa4c08c31
Gaurav Saral <gsaral@microsoft.com>
2019-01-16 09:39:56 +0530

    UT to test if keyring installation works or not

--- azure-devops/azext_devops/test/common/test_pip_helper.py
+++ azure-devops/azext_devops/test/common/test_pip_helper.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 import unittest
+from azext_devops.dev.common.pip_helper import install_keyring
 
 class TestPipHelperMethods(unittest.TestCase):
 
@@ -13,6 +14,24 @@ class TestPipHelperMethods(unittest.TestCase):
         except ImportError:
             self.fail('dependency on cli core is broken, pip helper will need fix')
 
+    def test_keyring_install_working(self):
+        install_keyring()
+        # uninstall keyring
+        pip_args = ['uninstall', 'keyring', '-y']
+        from azure.cli.core.extension.operations import _run_pip
+        pip_status_code = _run_pip(pip_args)  # pylint: disable=protected-access
+        if pip_status_code > 0:
+            self.fail('keyring uninstall failed')
+
+        try:
+            import keyring
+            self.fail('importing keyring should have failed')
+        except ImportError:
+            print('expected')
+
+        # install keyring and make sure we can import that
+        install_keyring()
+        import keyring
 
 if __name__ == '__main__':
     unittest.main()
