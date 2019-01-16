0361d4f9659da4d738891d2b5852bb4d248cbcaa
Ishita Mehta <43136080+ishitam8@users.noreply.github.com>
2019-01-16 18:18:45 +0530

    [WIP ]Updating dev setup script (#327)
    Updating dev setup script

--- scripts/dev_setup.py
+++ scripts/dev_setup.py
@@ -8,9 +8,13 @@ import os
 from subprocess import check_call, CalledProcessError
 
 root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..'))
+extension_dir = os.path.join(root_dir,'azure-devops')
+azure_dir = os.getenv('AZURE_CONFIG_DIR', None) or os.path.expanduser(os.path.join('~', '.azure'))
 
 print('Running dev setup...')
 print('Root directory \'{}\'\n'.format(root_dir))
+print('Extension directory \'{}\'\n'.format(extension_dir))
+print('Azure root directory \'{}\'\n'.format(azure_dir))
 
 def exec_command(command):
     try:
@@ -21,11 +25,16 @@ def exec_command(command):
         print(err, file=sys.stderr)
         sys.exit(1)
 
+
 # install general requirements and azure-cli
 exec_command('python -m pip install --upgrade pip')
 
-if os.path.isfile('./requirements.txt'):
-    exec_command('pip install -r requirements.txt')
+# install to edge build of azure-cli
+exec_command('pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge --no-cache-dir')
 
-# upgrade to latest az-dev-cli
-exec_command(r'pip install --upgrade .\azure-devops')
+os.environ['AZURE_EXTENSION_DIR'] = os.path.join(azure_dir,'devcliextensions')
+
+exec_command('pip install -e {}'.format(extension_dir))
+exec_command('pip install --upgrade --target {0}/devcliextensions/azure-devops {1}'.format(azure_dir, extension_dir))
+
+print('Finished dev setup.')
