9990404613bf866371fe96eeeed32d228aa0da53
Gaurav Saral <gsaral@microsoft.com>
2019-01-17 15:47:28 +0530

    fail in case some style check fails

--- scripts/runStyleCheck.ps1
+++ scripts/runStyleCheck.ps1
@@ -1,5 +1,7 @@
 scripts/setupCiMachine.ps1
 
+$failure = $false
+
 Write-Output "Installing azure devops python sdk"
 pip install vsts                     
 
@@ -11,9 +13,22 @@ pip install flake8
 
 Write-Output "Running pylint on all source"
 pylint --rcfile pylintrc ./azure-devops/azext_devops -f colorized
+
+if ($LastExitCode -ne 0) {
+    $failure = $true
+  }
+
 Write-Output "Pylint OK!"
 
 Write-Output "Running flake8 checks"
 flake8 --config .flake8
+
+if ($LastExitCode -ne 0) {
+    $failure = $true
+  }
+
 Write-Output "Flake8 OK!"
 
+if($failure -eq $true){
+    exit 1
+}
