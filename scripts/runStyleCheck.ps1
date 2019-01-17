048f29af9776783e26deff33f11d42c6a75013d2
Gaurav Saral <gsaral@microsoft.com>
2019-01-17 16:02:07 +0530

    minor

--- scripts/runStyleCheck.ps1
+++ scripts/runStyleCheck.ps1
@@ -16,18 +16,20 @@ pylint --rcfile pylintrc ./azure-devops/azext_devops -f colorized
 
 if ($LastExitCode -ne 0) {
     $failure = $true
+    Write-Output "Pylint NOT OK!"
+  } else {
+    Write-Output "Pylint OK!"
   }
-
-Write-Output "Pylint OK!"
 
 Write-Output "Running flake8 checks"
 flake8 --config .flake8
 
 if ($LastExitCode -ne 0) {
     $failure = $true
+    Write-Output "flake8 NOT OK!"
+  } else {
+    Write-Output "flake8 OK!"
   }
-
-Write-Output "Flake8 OK!"
 
 if($failure -eq $true){
     exit 1
