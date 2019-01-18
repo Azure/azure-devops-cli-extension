b2ba236031fcb6f2188215ebb4a800a4f4dd77bd
Gaurav Saral <gsaral@microsoft.com>
2019-01-18 14:58:26 +0530

    use Id instead of message Id in banner commands

--- azure-devops/azext_devops/dev/admin/arguments.py
+++ azure-devops/azext_devops/dev/admin/arguments.py
@@ -17,5 +17,4 @@ def load_admin_arguments(self, _):
         context.argument('access_level', **enum_choice_list(_ORGANIZATION_LICENSE_TYPES_FOR_ADD))
     with self.argument_context('devops admin banner') as context:
         context.argument('message', options_list=['-m', '--message'])
-        context.argument('message_id', options_list='--id')
         context.argument('banner_type', options_list=['-t', '--type'], **enum_choice_list(_TYPE_FOR_BANNER))
--- azure-devops/azext_devops/dev/admin/banner.py
+++ azure-devops/azext_devops/dev/admin/banner.py
@@ -24,10 +24,10 @@ def banner_list(devops_organization=None, detect=None):
         raise CLIError(ex)
 
 
-def banner_show(message_id, devops_organization=None, detect=None):
+def banner_show(id, devops_organization=None, detect=None):
     """Show details for a banner.
-    :param message_id: Identifier for the banner.
-    :type message_id: str
+    :param id: Identifier for the banner.
+    :type id: str
     :param devops_organization: Azure Devops organization URL. Example: https://dev.azure.com/MyOrganizationName/
     :type devops_organization: str
     :param detect: Automatically detect organization and project. Default is "on".
@@ -37,22 +37,22 @@ def banner_show(message_id, devops_organization=None, detect=None):
     try:
         existing_entries = setting_list(user_scope='host', key=GLOBAL_MESSAGE_BANNERS_KEY,
                                         devops_organization=devops_organization, detect=detect)
-        if message_id not in existing_entries:
-            raise ValueError('The following banner was not found: %s' % message_id)
-        return {message_id: existing_entries[message_id]}
+        if id not in existing_entries:
+            raise ValueError('The following banner was not found: %s' % id)
+        return {id: existing_entries[id]}
     except VstsServiceError as ex:
         raise CLIError(ex)
 
 
-def banner_add(message, banner_type=None, message_id=None, expiration=None, devops_organization=None, detect=None):
+def banner_add(message, banner_type=None, id=None, expiration=None, devops_organization=None, detect=None):
     """Add a new banner and immediately show it.
     :param message: Message (string) to show in the banner.
     :type message: str
     :param banner_type: Type of banner to present. Defaults is "info".
     :type banner_type: str
-    :param message_id: Identifier for the new banner. This identifier is needed to change or remove the message later. A
+    :param id: Identifier for the new banner. This identifier is needed to change or remove the message later. A
                        unique identifier is automatically created if one is not specified.
-    :type message_id: str
+    :type id: str
     :param expiration: Date/time when the banner should no longer be presented to users. If not set, the banner does not
                        automatically expire and must be removed with the remove command.
     :type expiration: date
@@ -67,10 +67,10 @@ def banner_add(message, banner_type=None, message_id=None, expiration=None, devo
             expiration_iso8601 = convert_date_string_to_iso8601(value=expiration, argument='expiration')
         else:
             expiration_iso8601 = None
-        if message_id is None or message_id == '':
+        if id is None or id == '':
             import uuid
-            message_id = str(uuid.uuid4())
-        setting_key = _get_banner_key(message_id)
+            id = str(uuid.uuid4())
+        setting_key = _get_banner_key(id)
         entries = {
             setting_key: {
                 "message": message
@@ -82,20 +82,20 @@ def banner_add(message, banner_type=None, message_id=None, expiration=None, devo
             entries[setting_key]['expirationDate'] = expiration_iso8601
         setting_add_or_update(entries=entries, user_scope=USER_SCOPE_HOST,
                               devops_organization=devops_organization, detect=detect)
-        return {message_id: entries[setting_key]}
+        return {id: entries[setting_key]}
     except VstsServiceError as ex:
         raise CLIError(ex)
 
 
-def banner_update(message=None, banner_type=None, message_id=None, expiration=None, devops_organization=None,
+def banner_update(message=None, banner_type=None, id=None, expiration=None, devops_organization=None,
                   detect=None):
     """Update the message, level, or expiration date for a banner.
     :param message: Message (string) to show in the banner.
     :type message: str
     :param banner_type: Type of banner to present. Defaults is "info".
     :type banner_type: str
-    :param message_id: ID of the banner to update.
-    :type message_id: str
+    :param id: ID of the banner to update.
+    :type id: str
     :param expiration: Date/time when the banner should no longer be presented to users. To unset the expiration for the
                        banner, supply an empty value to this argument.
     :type expiration: date
@@ -117,10 +117,10 @@ def banner_update(message=None, banner_type=None, message_id=None, expiration=No
                                         key=GLOBAL_MESSAGE_BANNERS_KEY,
                                         devops_organization=devops_organization,
                                         detect=detect)
-        if message_id not in existing_entries:
-            raise ValueError('The following banner was not found: %s' % message_id)
-        existing_entry = existing_entries[message_id]
-        setting_key = _get_banner_key(message_id)
+        if id not in existing_entries:
+            raise ValueError('The following banner was not found: %s' % id)
+        existing_entry = existing_entries[id]
+        setting_key = _get_banner_key(id)
         entries = {
             setting_key: {
                 "message": message
@@ -143,15 +143,15 @@ def banner_update(message=None, banner_type=None, message_id=None, expiration=No
 
         setting_add_or_update(entries=entries, user_scope=USER_SCOPE_HOST, devops_organization=devops_organization,
                               detect=detect)
-        return {message_id: entries[setting_key]}
+        return {id: entries[setting_key]}
     except VstsServiceError as ex:
         raise CLIError(ex)
 
 
-def banner_remove(message_id, devops_organization=None, detect=None):
+def banner_remove(id, devops_organization=None, detect=None):
     """Remove a banner.
-    :param message_id: ID of the banner to remove.
-    :type message_id: str
+    :param id: ID of the banner to remove.
+    :type id: str
     :param devops_organization: Azure Devops organization URL. Example: https://dev.azure.com/MyOrganizationName/
     :type devops_organization: str
     :param detect: Automatically detect organization and project. Default is "on".
@@ -159,7 +159,7 @@ def banner_remove(message_id, devops_organization=None, detect=None):
     :rtype: [object]
     """
     try:
-        setting_key = _get_banner_key(message_id)
+        setting_key = _get_banner_key(id)
         setting_remove(key=setting_key, user_scope='host', devops_organization=devops_organization, detect=detect)
     except VstsServiceError as ex:
         raise CLIError(ex)
