442c17c9cfa0df2608218a4d0c87d1e9aa3d1658
Ishita Mehta <ishitam@microsoft.com>
2019-01-21 19:51:11 +0530

    Fixing team UTs

--- azure-devops/azext_devops/test/common/test_credentials.py
+++ azure-devops/azext_devops/test/common/test_credentials.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import pdb
 import os
 import unittest
 import uuid
