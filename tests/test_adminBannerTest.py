a55b9312b3add37d98e3ce3fc5ec0a4a54c1d28f
Gaurav Saral <gsaral@microsoft.com>
2019-01-24 09:58:24 +0530

    really fixing test cases

--- tests/test_adminBannerTest.py
+++ tests/test_adminBannerTest.py
@@ -8,8 +8,7 @@ import unittest
 
 from azure.cli.testsdk import ScenarioTest
 from azure_devtools.scenario_tests import AllowLargeResponse
-from .utilities.helper import ( DEVOPS_CLI_TEST_ORGANIZATION , DEVOPS_CLI_TEST_PAT_TOKEN, disable_telemetry )
-from azext_devops.dev.common.const import PAT_ENV_VARIABLE_NAME
+from .utilities.helper import ( DEVOPS_CLI_TEST_ORGANIZATION , DEVOPS_CLI_TEST_PAT_TOKEN, disable_telemetry, PAT_ENV_VARIABLE_NAME )
 import time
 
 class AdminBannerTests(ScenarioTest):
