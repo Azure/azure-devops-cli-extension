510a05e2af89857aa62c7497603570464eee5d86
Gaurav Saral <gsaral@microsoft.com>
2019-01-24 09:33:36 +0530

    trying to put this check in pr pipeline

---
+++ scripts/findAndInstallExtensionFromPath.ps1
@@ -0,0 +1,6 @@
+param([string]$pathToProbe)
+
+Write-Host "installing azure dev cli extension"
+$extensions = Get-ChildItem -Path $pathToProbe -Filter "*.whl" -Recurse | Select-Object FullName
+az extension add --source $extensions[0].FullName -y
+Write-Host "done"
