6fd30069325b5e1152e2a315765a4a227d28f953
Atul Bagga <atbagga@microsoft.com>
2019-01-24 15:09:00 +0530

    Support ssh url parsing in git repo auto detect

--- azure-devops/azext_devops/dev/common/uri.py
+++ azure-devops/azext_devops/dev/common/uri.py
@@ -11,13 +11,21 @@ except ImportError:
 
 
 def uri_parse(url):
-    return urlparse(url)
+    # Special handling for NEW ssh urls which do not start with ssh://
+    if not url.startswith('ssh:') and ("vs-ssh.visualstudio.com" in url or "ssh.dev.azure.com" in url):
+        # e.g. org@vs-ssh.visualstudio.com:v3/org/project/repo
+        # e.g. git@ssh.dev.azure.com:v3/org/Project/Repo
+        # append ssh at start to set correct scheme
+        return urlparse("ssh://{original_uri}".format(original_uri=url))
+    else:
+        return urlparse(url)
 
 
 def uri_quote(query_data):
     return quote(query_data)
 
 
+# Only works for hosted scenario
 def uri_parse_instance_from_git_uri(uri):
     if "/_git" in uri:
         parsed_uri = urlparse(uri)
