a726ae54ee26f2c76b079391a4a21a885b5d5a83
Gaurav Saral <gsaral@microsoft.com>
2019-01-25 15:34:38 +0530

    UTs done

--- azure-devops/azext_devops/test/repos/test_policy.py
+++ azure-devops/azext_devops/test/repos/test_policy.py
@@ -28,11 +28,13 @@ class TestUuidMethods(unittest.TestCase):
         self.get_policy_patcher = patch('vsts.policy.v4_0.policy_client.PolicyClient.get_policy_configuration')
         self.delete_policy_patcher = patch('vsts.policy.v4_0.policy_client.PolicyClient.delete_policy_configuration')
         self.create_policy_patcher = patch('vsts.policy.v4_0.policy_client.PolicyClient.create_policy_configuration')
+        self.update_policy_patcher = patch('vsts.policy.v4_0.policy_client.PolicyClient.update_policy_configuration')
 
         self.mock_get_policies = self.get_policies_patcher.start()
         self.mock_get_policy = self.get_policy_patcher.start()
         self.mock_delete_policy = self.delete_policy_patcher.start()
         self.mock_create_policy = self.create_policy_patcher.start()
+        self.mock_update_policy = self.update_policy_patcher.start()
 
         clear_connection_cache()
 
@@ -41,6 +43,7 @@ class TestUuidMethods(unittest.TestCase):
         self.mock_get_policy.stop()
         self.mock_delete_policy.stop()
         self.mock_create_policy.stop()
+        self.mock_update_policy.stop()
 
     def test_name_of_array(self):
         first_name = 'a'
@@ -92,11 +95,11 @@ class TestUuidMethods(unittest.TestCase):
 
     def test_create_policy_scope(self):
         create_policy(repository_id=self._TEST_REPOSITORY_ID, branch='master',
-            policy_type='ApproverCountPolicy',
-            minimumApproverCount=2, creatorVoteCounts= True, allowDownvotes= False, resetOnSourcePush= True,
-            organization = self._TEST_DEVOPS_ORGANIZATION,
-            project = self._TEST_DEVOPS_PROJECT,
-            detect='off')
+        policy_type='ApproverCountPolicy',
+        minimumApproverCount=2, creatorVoteCounts= True, allowDownvotes= False, resetOnSourcePush= True,
+        organization = self._TEST_DEVOPS_ORGANIZATION,
+        project = self._TEST_DEVOPS_PROJECT,
+        detect='off')
 
         self.mock_create_policy.assert_called_once()
         create_policy_object = self.mock_create_policy.call_args_list[0][1]
@@ -108,11 +111,11 @@ class TestUuidMethods(unittest.TestCase):
 
     def test_create_policy_setting_creation(self):
         create_policy(repository_id=self._TEST_REPOSITORY_ID, branch='master',
-            policy_type='ApproverCountPolicy',
-            minimumApproverCount=2, creatorVoteCounts= True, allowDownvotes= False, resetOnSourcePush= True,
-            organization = self._TEST_DEVOPS_ORGANIZATION,
-            project = self._TEST_DEVOPS_PROJECT,
-            detect='off')
+        policy_type='ApproverCountPolicy',
+        minimumApproverCount=2, creatorVoteCounts= True, allowDownvotes= False, resetOnSourcePush= True,
+        organization = self._TEST_DEVOPS_ORGANIZATION,
+        project = self._TEST_DEVOPS_PROJECT,
+        detect='off')
 
         self.mock_create_policy.assert_called_once()
         create_policy_object = self.mock_create_policy.call_args_list[0][1]
@@ -125,11 +128,11 @@ class TestUuidMethods(unittest.TestCase):
 
     def test_create_policy_id_assignment(self):
         create_policy(repository_id=self._TEST_REPOSITORY_ID, branch='master',
-            policy_type='ApproverCountPolicy',
-            minimumApproverCount=2, creatorVoteCounts= True, allowDownvotes= False, resetOnSourcePush= True,
-            organization = self._TEST_DEVOPS_ORGANIZATION,
-            project = self._TEST_DEVOPS_PROJECT,
-            detect='off')
+        policy_type='ApproverCountPolicy',
+        minimumApproverCount=2, creatorVoteCounts= True, allowDownvotes= False, resetOnSourcePush= True,
+        organization = self._TEST_DEVOPS_ORGANIZATION,
+        project = self._TEST_DEVOPS_PROJECT,
+        detect='off')
 
         self.mock_create_policy.assert_called_once()
         create_policy_object = self.mock_create_policy.call_args_list[0][1]
@@ -137,7 +140,18 @@ class TestUuidMethods(unittest.TestCase):
         policy_type_id = create_policy_object['configuration'].type['id']  # 0 because we set only only scope from CLI
         self.assertEqual(policy_type_id, 'fa4e907d-c16b-4a4c-9dfa-4906e5d171dd')
 
-        
+    def test_update_policy(self):
+        update_policy(repository_id=self._TEST_REPOSITORY_ID, branch='master',
+        policy_id = 121,
+        policy_type='ApproverCountPolicy',
+        minimumApproverCount=2, creatorVoteCounts= True, allowDownvotes= False, resetOnSourcePush= True,
+        organization = self._TEST_DEVOPS_ORGANIZATION,
+        project = self._TEST_DEVOPS_PROJECT,
+        detect='off')
+
+        self.mock_update_policy.assert_called_once()
+        update_policy_object = self.mock_update_policy.call_args_list[0][1]
+        self.assertEqual(update_policy_object['configuration_id'], 121)        
 
 if __name__ == '__main__':
     unittest.main()
