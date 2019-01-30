7e7a30d4342ee589390760840b0115d28b439f8b
gauravsaralms <43203065+gauravsaralMs@users.noreply.github.com>
2019-01-30 15:57:10 +0530

    Recording Test for policy Commands (#393)

--- azure-devops/azext_devops/dev/repos/arguments.py
+++ azure-devops/azext_devops/dev/repos/arguments.py
@@ -22,6 +22,11 @@ def load_code_arguments(self, _):
 
     with self.argument_context('repos policies create') as context:
         context.argument('policy_type', **enum_choice_list(REPO_POLICY_TYPE))
+        context.argument('repository_id', options_list=('--repository-id', '-r'))
+
+    with self.argument_context('repos policies update') as context:
+        context.argument('policy_type', **enum_choice_list(REPO_POLICY_TYPE))
+        context.argument('repository_id', options_list=('--repository-id', '-r'))
 
     with self.argument_context('repos pr') as context:
         context.argument('description', type=str, options_list=('--description', '-d'), nargs='*')
