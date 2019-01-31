8c291e2aa1b98541a0e4dcaa6ff8215aafcccfdc
Mathivanan Palanisamy <mathy@outlook.com>
2019-01-31 17:52:19 +0000

    Added unit tests and corrected a styling issue

--- tests/test_pipelinesBuildTagTest.py
+++ tests/test_pipelinesBuildTagTest.py
@@ -8,9 +8,12 @@ import unittest
 
 from azure.cli.testsdk import ScenarioTest
 from azure_devtools.scenario_tests import AllowLargeResponse
-from .utilities.helper import DEVOPS_CLI_TEST_ORGANIZATION, disable_telemetry, set_authentication
+from .utilities.helper import disable_telemetry, set_authentication, get_test_org_from_env_variable
+
+DEVOPS_CLI_TEST_ORGANIZATION = get_test_org_from_env_variable() or 'https://dev.azure.com/dhilmathy'
 
 class PipelinesBuildTagTests(ScenarioTest):    
+    @AllowLargeResponse(size_kb=3072)
     @disable_telemetry
     @set_authentication
     def test_build_tag_listAddDelete(self):
@@ -31,21 +34,21 @@ class PipelinesBuildTagTests(ScenarioTest):
             assert add_build_tag_output == ['TestTag0']
 
             #Add build tags to queued build
-            add_build_tag_command = 'az pipelines build tag add --build-id ' + str(queued_build_id) + ' --tags TestTag1,TestTag2 --detect off --output json'
+            add_build_tag_command = 'az pipelines build tag add --build-id ' + str(queued_build_id) + ' --tags "TestTag1,TestTag2" --detect off --output json'
             add_build_tag_output = self.cmd(add_build_tag_command).get_output_in_json()
             assert len(add_build_tag_output) == 3
             assert add_build_tag_output == ['TestTag0', 'TestTag1', "TestTag2"]
 
             #List build tags added to queued build
-            show_build_tag_command = 'az pipelines build tag show --build-id ' + str(queued_build_id) + ' --detect off --output json'
-            show_build_tag_output = self.cmd(show_build_tag_command).get_output_in_json()
-            assert len(show_build_tag_output) == 3
-            assert show_build_tag_output == ['TestTag0', 'TestTag1', "TestTag2"]
+            list_build_tag_command = 'az pipelines build tag list --build-id ' + str(queued_build_id) + ' --detect off --output json'
+            list_build_tag_output = self.cmd(list_build_tag_command).get_output_in_json()
+            assert len(list_build_tag_output) == 3
+            assert list_build_tag_output == ['TestTag0', 'TestTag1', "TestTag2"]
 
             #Delete build tag from queued build
             delete_build_tag_command = 'az pipelines build tag delete --build-id ' + str(queued_build_id) + ' --tag TestTag2 --detect off --output json'
             delete_build_tag_output = self.cmd(delete_build_tag_command).get_output_in_json()
-            assert len(delete_build_tag_output) == 3
+            assert len(delete_build_tag_output) == 2
             assert delete_build_tag_output == ['TestTag0', 'TestTag1']
         finally:
             #Delete tags added for test
@@ -55,7 +58,7 @@ class PipelinesBuildTagTests(ScenarioTest):
             self.cmd(delete_build_tag_command)
 
             #Verify deletion
-            show_build_tag_command = 'az pipelines build tag show --build-id ' + str(queued_build_id) + ' --detect off --output json'
-            show_build_tag_output = self.cmd(show_build_tag_command).get_output_in_json()
-            if (len(show_build_tag_output) == 0):
-                assert 0
+            list_build_tag_command = 'az pipelines build tag list --build-id ' + str(queued_build_id) + ' --detect off --output json'
+            add_build_tag_output = self.cmd(list_build_tag_command).get_output_in_json()
+            if (len(add_build_tag_output) == 0):
+                assert 1
