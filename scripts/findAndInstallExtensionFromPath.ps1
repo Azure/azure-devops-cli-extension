94038481c477f3eb1efcff1d01997928a5e8a77a
Gaurav Saral <gsaral@microsoft.com>
2019-02-04 10:00:33 +0530

    some change

--- scripts/findAndInstallExtensionFromPath.ps1
+++ scripts/findAndInstallExtensionFromPath.ps1
@@ -1,7 +1,9 @@
 param([string]$pathToProbe)
 
 Write-host "we will be probing " $pathToProbe
-Write-Host "installing azure dev cli extension"
 $extensions = Get-ChildItem -Path $pathToProbe -Filter "*.whl" -Recurse | Select-Object FullName
-az extension add --source $extensions[0].FullName -y
+for($extension in $extensions)
+{
+    az extension add --source $extension.FullName -y
+}
 Write-Host "done"
