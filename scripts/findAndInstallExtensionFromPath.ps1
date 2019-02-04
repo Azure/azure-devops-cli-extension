95b20b0a02e403345b1af045d16db0b92b52ca37
Gaurav Saral <gsaral@microsoft.com>
2019-02-04 14:23:49 +0530

    minor update

--- scripts/findAndInstallExtensionFromPath.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$pathToProbe)
-
-Write-host "we will be probing " $pathToProbe
-$extensions = Get-ChildItem -Path $pathToProbe -Filter "*.whl" -Recurse | Select-Object FullName
-Foreach ($extension in $extensions)
-{
-    az extension add --source $extension.FullName -y
-}
-Write-Host "done"
--- scripts/findAndInstallWheelsFromPath.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$pathToProbe)
-
-Write-host "we will be probing " $pathToProbe
-$extensions = Get-ChildItem -Path $pathToProbe -Filter "*.whl" -Recurse | Select-Object FullName
-Foreach ($extension in $extensions)
-{
-    pip install $extension.FullName
-}
-Write-Host "done"
