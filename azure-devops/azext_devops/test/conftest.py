5422a00437ea1bd7462147953772a541809113dd
Gaurav Saral <gsaral@microsoft.com>
2019-02-08 09:27:49 +0530

    minor

--- azure-devops/azext_devops/test/conftest.py
+++ azure-devops/azext_devops/test/conftest.py
@@ -6,7 +6,7 @@
 
 import sys
 from azure.cli.core.extension import get_extension_path
-# Make sure that the application source directory (this directory's parent) is on sys.path.
+# Make sure that the extension install directory is on sys.path so that dependencies can be found.
 
 extensionPath = get_extension_path('azure-devops')
 sys.path.append(extensionPath)
