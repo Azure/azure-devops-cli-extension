deb2006df422a723e8bdf90cdec33c85835f7d4d
Gaurav Saral <gsaral@microsoft.com>
2019-02-11 11:46:37 +0530

    fix pylint in utils.helper

--- azure-devops/azext_devops/test/utils/helper.py
+++ azure-devops/azext_devops/test/utils/helper.py
@@ -23,31 +23,33 @@ def get_client_mock_helper(_self_dummy, client_type):
     from azext_devops.vstsCompressed.location.v4_0.location_client import LocationClient
     from azext_devops.vstsCompressed.build.v4_0.build_client import BuildClient
 
+    vsts = 'azext_devops.vstsCompressed.'
+
     switcher = {
-        'azext_devops.vstsCompressed.git.v4_0.git_client.GitClient': GitClient(base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.policy.v4_0.policy_client.PolicyClient': PolicyClient(base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.core.v4_0.core_client.CoreClient': CoreClient(base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.release.v4_0.release_client.ReleaseClient': ReleaseClient(base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.customer_intelligence.v4_0.customer_intelligence_client.CustomerIntelligenceClient': \
+        vsts+'git.v4_0.git_client.GitClient': GitClient(base_url=TEST_DEVOPS_ORG_URL),
+        vsts+'policy.v4_0.policy_client.PolicyClient': PolicyClient(base_url=TEST_DEVOPS_ORG_URL),
+        vsts+'core.v4_0.core_client.CoreClient': CoreClient(base_url=TEST_DEVOPS_ORG_URL),
+        vsts+'release.v4_0.release_client.ReleaseClient': ReleaseClient(base_url=TEST_DEVOPS_ORG_URL),
+        vsts+'customer_intelligence.v4_0.customer_intelligence_client.CustomerIntelligenceClient': \
             CustomerIntelligenceClient(base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.service_endpoint.v4_1.service_endpoint_client.ServiceEndpointClient': ServiceEndpointClient(
+        vsts+'service_endpoint.v4_1.service_endpoint_client.ServiceEndpointClient': ServiceEndpointClient(
             base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.operations.v4_0.operations_client.OperationsClient': OperationsClient(
+        vsts+'operations.v4_0.operations_client.OperationsClient': OperationsClient(
             base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.task_agent.v4_0.task_agent_client.TaskAgentClient': TaskAgentClient(
+        vsts+'task_agent.v4_0.task_agent_client.TaskAgentClient': TaskAgentClient(
             base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.work_item_tracking.v4_0.work_item_tracking_client.WorkItemTrackingClient': WorkItemTrackingClient(
+        vsts+'work_item_tracking.v4_0.work_item_tracking_client.WorkItemTrackingClient': WorkItemTrackingClient(
             base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.settings.v4_0.settings_client.SettingsClient': SettingsClient(
+        vsts+'settings.v4_0.settings_client.SettingsClient': SettingsClient(
             base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.identity.v4_0.identity_client.IdentityClient': IdentityClient(
+        vsts+'identity.v4_0.identity_client.IdentityClient': IdentityClient(
             base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.member_entitlement_management.v4_1.member_entitlement_management_client.  \
+        vsts+'member_entitlement_management.v4_1.member_entitlement_management_client.  \
             MemberEntitlementManagementClient': MemberEntitlementManagementClient(
                 base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.location.v4_0.location_client.LocationClient': LocationClient(
+        vsts+'location.v4_0.location_client.LocationClient': LocationClient(
             base_url=TEST_DEVOPS_ORG_URL),
-        'azext_devops.vstsCompressed.build.v4_0.build_client.BuildClient': BuildClient(base_url=TEST_DEVOPS_ORG_URL)
+        vsts+'build.v4_0.build_client.BuildClient': BuildClient(base_url=TEST_DEVOPS_ORG_URL)
     }
 
     return switcher.get(client_type, None)
