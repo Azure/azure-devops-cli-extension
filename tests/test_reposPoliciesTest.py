1116e0162a93eca31bde0d2d19d8df7b2218b969
Gaurav Saral <gsaral@microsoft.com>
2019-02-14 16:48:25 +0530

    update for policy

--- tests/test_reposPoliciesTest.py
+++ tests/test_reposPoliciesTest.py
@@ -10,7 +10,7 @@ from azure.cli.testsdk import ScenarioTest
 from azure_devtools.scenario_tests import AllowLargeResponse
 from .utilities.helper import disable_telemetry, set_authentication, get_test_org_from_env_variable
 
-DEVOPS_CLI_TEST_ORGANIZATION = get_test_org_from_env_variable() or 'Https://dev.azure.com/baggaatul24'
+DEVOPS_CLI_TEST_ORGANIZATION = get_test_org_from_env_variable() or 'Https://dev.azure.com/azuredevopsclitest'
 
 class DevopsReposPoliciesTests(ScenarioTest):
     @AllowLargeResponse(size_kb=3072)
