1dff1a20c5f427fc26603a8a8372bd58f65e31eb
Gaurav Saral <gsaral@microsoft.com>
2019-02-18 14:04:23 +0530

    clean up policy create/ delete

--- azure-devops/azext_devops/dev/repos/arguments.py
+++ azure-devops/azext_devops/dev/repos/arguments.py
@@ -13,40 +13,6 @@ _VOTE_VALUES = ['approve', 'approve-with-suggestions', 'reset', 'wait-for-author
 _PR_STATUS_VALUES = ['all', 'active', 'completed', 'abandoned']
 
 
-def repo_policy_create_udpate_common_arguments(context):
-    context.argument('is_blocking', **enum_choice_list(_TRUE_FALSE_SWITCH))
-    context.argument('is_enabled', **enum_choice_list(_TRUE_FALSE_SWITCH))
-    context.argument('policy_type', **enum_choice_list(REPO_POLICY_TYPE))
-    context.argument('repository_id', options_list=('--repository-id', '-r'))
-    APPROVER_COUNT_POLICY_ARGUMENT_GROUP = 'Approver Count Policy'
-    context.argument('minimum_approver_count', arg_group=APPROVER_COUNT_POLICY_ARGUMENT_GROUP)
-    context.argument('creator_vote_counts', arg_group=APPROVER_COUNT_POLICY_ARGUMENT_GROUP,
-                     **enum_choice_list(_TRUE_FALSE_SWITCH))
-    context.argument('allow_downvotes', arg_group=APPROVER_COUNT_POLICY_ARGUMENT_GROUP,
-                     **enum_choice_list(_TRUE_FALSE_SWITCH))
-    context.argument('reset_on_source_push', arg_group=APPROVER_COUNT_POLICY_ARGUMENT_GROUP,
-                     **enum_choice_list(_TRUE_FALSE_SWITCH))
-    MERGE_STRATEGY_POLICY_ARGUMENT_GROUP = 'Merge Strategy Policy'
-    context.argument('use_squash_merge', arg_group=MERGE_STRATEGY_POLICY_ARGUMENT_GROUP,
-                     **enum_choice_list(_TRUE_FALSE_SWITCH))
-    BUILD_POLICY_ARGUMENT_GROUP = 'Build Policy'
-    context.argument('build_definition_id', arg_group=BUILD_POLICY_ARGUMENT_GROUP)
-    context.argument('queue_on_source_update_only', arg_group=BUILD_POLICY_ARGUMENT_GROUP,
-                     **enum_choice_list(_TRUE_FALSE_SWITCH))
-    context.argument('manual_queue_only', arg_group=BUILD_POLICY_ARGUMENT_GROUP,
-                     **enum_choice_list(_TRUE_FALSE_SWITCH))
-    context.argument('display_name', arg_group=BUILD_POLICY_ARGUMENT_GROUP)
-    context.argument('valid_duration', arg_group=BUILD_POLICY_ARGUMENT_GROUP)
-    FILE_SIZE_POLICY_ARGUMENT_GROUP = 'File Size Policy'
-    context.argument('maximum_git_blob_size_in_bytes', arg_group=FILE_SIZE_POLICY_ARGUMENT_GROUP)
-    context.argument('use_uncompressed_size', arg_group=FILE_SIZE_POLICY_ARGUMENT_GROUP,
-                     **enum_choice_list(_TRUE_FALSE_SWITCH))
-    REQUIRED_REVIEWER_POLICY_GROUP = 'Required Reviewer Policy'
-    context.argument('optional_reviewer_ids', arg_group=REQUIRED_REVIEWER_POLICY_GROUP)
-    context.argument('required_reviewer_ids', arg_group=REQUIRED_REVIEWER_POLICY_GROUP)
-    context.argument('message', arg_group=REQUIRED_REVIEWER_POLICY_GROUP)
-
-
 def load_code_arguments(self, _):
     with self.argument_context('repos') as context:
         context.argument('project', options_list=('--project', '-p'))
@@ -54,13 +20,6 @@ def load_code_arguments(self, _):
         context.argument('repository', options_list=('--repository', '-r'))
         context.argument('reviewers', nargs='*')
         context.argument('detect', **enum_choice_list(_ON_OFF_SWITCH_VALUES))
-
-    with self.argument_context('repos policy create') as context:
-        repo_policy_create_udpate_common_arguments(context)
-
-    with self.argument_context('repos policy update') as context:
-        repo_policy_create_udpate_common_arguments(context)
-        context.argument('policy_id', options_list=('--policy-id', '--id'))
 
     with self.argument_context('repos pr') as context:
         context.argument('description', type=str, options_list=('--description', '-d'), nargs='*')
