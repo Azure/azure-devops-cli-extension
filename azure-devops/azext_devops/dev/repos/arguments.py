8a7844197a25ef7641c288e9be56d76e74cbebc8
Gaurav Saral <gsaral@microsoft.com>
2019-02-18 14:59:25 +0530

    flagging

--- azure-devops/azext_devops/dev/repos/arguments.py
+++ azure-devops/azext_devops/dev/repos/arguments.py
@@ -29,8 +29,8 @@ def load_code_arguments(self, _):
         context.argument('project', options_list=('--project', '-p'),
                          help='Name or ID of the project.')
         context.argument('policy_configuration', options_list=('--policy-configuration', '--config'),
-                         help='File path of file containing policy configuration to create in a serialized form. ' +
-                         'please use / backslash when typing in directory path. '
+                         help='File path of file containing policy configuration to create in a serialized form. '
+                         +'please use / backslash when typing in directory path. '
                          +'Only --project and --organization param are needed when passing this.')
         context.argument('policy_id', options_list=('--policy-id', '--id'),
                          help='ID of the policy which needs to be updated')
