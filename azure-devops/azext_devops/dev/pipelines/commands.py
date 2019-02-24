296ff7011cf3e2a78cfc4c4da24112c114c27f5c
Mitch Denny <midenn@microsoft.com>
2019-02-23 21:33:59 -0800

    Skeleton of runs artifact commands.

--- azure-devops/azext_devops/dev/pipelines/commands.py
+++ azure-devops/azext_devops/dev/pipelines/commands.py
@@ -17,7 +17,6 @@ from ._format import (transform_build_table_output,
                       transform_release_definitions_table_output,
                       transform_release_definition_table_output)
 
-
 buildOps = CliCommandType(
     operations_tmpl='azext_devops.dev.pipelines.build#{}',
     exception_handler=azure_devops_exception_handler
@@ -41,6 +40,10 @@ releaseOps = CliCommandType(
 releaseDefinitionOps = CliCommandType(
     operations_tmpl='azext_devops.dev.pipelines.release_definition#{}',
     exception_handler=azure_devops_exception_handler
+)
+
+runOps = CliCommandType(
+    operations_tmpl='azext_devops.dev.pipelines.runs#{}'
 )
 
 
@@ -77,3 +80,8 @@ def load_build_commands(self, _):
         # basic release commands
         g.command('list', 'release_definition_list', table_transformer=transform_release_definitions_table_output)
         g.command('show', 'release_definition_show', table_transformer=transform_release_definition_table_output)
+
+    with self.command_group('pipelines runs artifact', command_type=runOps) as g:
+        g.command('download', 'run_artifact_download')
+        g.command('list', 'run_artifact_list')
+        g.command('upload', 'run_artifact_upload')
---
+++ azure-devops/azext_devops/dev/pipelines/runs.py
@@ -0,0 +1,51 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from webbrowser import open_new
+
+from knack.log import get_logger
+from knack.util import CLIError
+from azext_devops.vstsCompressed.exceptions import VstsServiceError
+from azext_devops.vstsCompressed.build.v4_0.models.models import Build
+from azext_devops.vstsCompressed.build.v4_0.models.models import DefinitionReference
+from azext_devops.dev.common.git import resolve_git_ref_heads
+from azext_devops.dev.common.identities import resolve_identity_as_id
+from azext_devops.dev.common.services import (get_build_client,
+                                              resolve_instance_and_project)
+from azext_devops.dev.common.uri import uri_quote
+
+logger = get_logger(__name__)
+
+def run_artifact_download(run_id=None, artifact_name=None, path=None, organization=None, project=None):
+    """Download a pipeline artifact.
+    :param run_id: ID of the run that the artifact is associated to.
+    :type run_id: int
+    :param artifact_name: Name of the artifact to download.
+    :type artifact_name: string
+    :param path: Path to download the artifact into.
+    :type path: string
+    """
+
+    print("Hello run_artifact_download")
+
+def run_artifact_list(run_id=None, organization=None, project=None):
+    """List artifacts associate with run.
+    :param run_id: ID of the run that the artifact is associated to.
+    :type run_id: int
+    """
+
+    print("Hello run_artifact_list")
+
+def run_artifact_upload(run_id=None, artifact_name=None, path=None, organization=None, project=None):
+    """Upload a pipeline artifact.
+    :param run_id: ID of the run that the artifact is associated to.
+    :type run_id: int
+    :param artifact_name: Name of the artifact to upload.
+    :type artifact_name: string
+    :param path: Path to upload the artifact from.
+    :type path: string
+    """
+
+    print("Hello run_artifact_upload")
