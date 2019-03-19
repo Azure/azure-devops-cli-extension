a3eeaafd8c2f9dc69bff260e80f6e0da6d7abe22
Gaurav Saral <gsaral@microsoft.com>
2019-03-19 16:16:08 +0530

    minor fixes

--- scripts/run_az.py
+++ scripts/run_az.py
@@ -7,4 +7,7 @@ import sys
 
 from azure.cli import __main__ as cli_main
 
+# If import here is failing then it means you need to install azure cli core in machine
+# pip install "git+https://github.com/Azure/azure-cli@dev#egg=azure-cli-core&subdirectory=src/azure-cli-core"
+
 sys.exit(cli_main(sys.argv))
