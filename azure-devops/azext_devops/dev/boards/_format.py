64349b35f7b48f86dfa542973d5910aeb8ab9802
Gaurav Saral <gsaral@microsoft.com>
2019-04-18 14:24:10 +0530

    table format

--- azure-devops/azext_devops/dev/boards/_format.py
+++ azure-devops/azext_devops/dev/boards/_format.py
@@ -22,6 +22,20 @@ def transform_work_item_relation_type_table_output(result):
     return table_output
 
 
+def transform_work_item_relations(result):
+    if result['relations'] is None:
+        return []
+
+    relations = result['relations']
+    table_output = []
+    for item in relations:
+        table_row = OrderedDict()
+        table_row['Relation Type'] = item['rel']
+        table_row['Url'] = item['url']
+        table_output.append(table_row)
+
+    return table_output
+
 def transform_work_items_table_output(result):
     table_output = []
     for item in result:
--- azure-devops/azext_devops/dev/boards/commands.py
+++ azure-devops/azext_devops/dev/boards/commands.py
@@ -8,7 +8,8 @@ from azure.cli.core.commands import CliCommandType
 from azext_devops.dev.common.exception_handler import azure_devops_exception_handler
 from ._format import (transform_work_item_table_output,
                       transform_work_item_query_result_table_output,
-                      transform_work_item_relation_type_table_output)
+                      transform_work_item_relation_type_table_output,
+                      transform_work_item_relations)
 
 
 workItemOps = CliCommandType(
@@ -38,6 +39,6 @@ def load_work_commands(self, _):
         # relation commands
         g.command('relation list-type', 'get_relation_types_show',
                   table_transformer=transform_work_item_relation_type_table_output)
-        g.command('relation add', 'add_relation', table_transformer=transform_work_item_table_output)
-        g.command('relation remove', 'remove_relation', table_transformer=transform_work_item_table_output,
+        g.command('relation add', 'add_relation', table_transformer=transform_work_item_relations)
+        g.command('relation remove', 'remove_relation', table_transformer=transform_work_item_relations,
                   confirmation='Are you sure you want to remove this relation(s)?')
