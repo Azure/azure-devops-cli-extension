416841aa841e807e551a259a8c71899a9832ba63
gauravsaralms <43203065+gauravsaralMs@users.noreply.github.com>
2019-04-22 11:32:31 +0530

    Work Item Relation Commands (#541)

--- azure-devops/azext_devops/dev/boards/arguments.py
+++ azure-devops/azext_devops/dev/boards/arguments.py
@@ -20,3 +20,14 @@ def load_work_arguments(self, _):
     with self.argument_context('boards work-item delete') as context:
         context.argument('yes', options_list=['--yes', '-y'], action='store_true',
                          help='Do not prompt for confirmation.')
+
+    with self.argument_context('boards work-item relation') as context:
+        context.argument('id', help='The ID of the work item')
+        context.argument('target_id', help='ID(s) of work-items to create relation with. \
+                         Multiple values can be passed comma separated. Example: 1,2 ')
+
+    with self.argument_context('boards work-item relation add') as context:
+        context.argument('relation_type', help='Relation type to create. example: parent, child ')
+
+    with self.argument_context('boards work-item relation remove') as context:
+        context.argument('relation_type', help='Relation type to remove. example: parent, child ')
--- azure-devops/azext_devops/dev/boards/commands.py
+++ azure-devops/azext_devops/dev/boards/commands.py
@@ -7,11 +7,18 @@
 from azure.cli.core.commands import CliCommandType
 from azext_devops.dev.common.exception_handler import azure_devops_exception_handler
 from ._format import (transform_work_item_table_output,
-                      transform_work_item_query_result_table_output)
+                      transform_work_item_query_result_table_output,
+                      transform_work_item_relation_type_table_output,
+                      transform_work_item_relations)
 
 
 workItemOps = CliCommandType(
     operations_tmpl='azext_devops.dev.boards.work_item#{}',
+    exception_handler=azure_devops_exception_handler
+)
+
+relationsOps = CliCommandType(
+    operations_tmpl='azext_devops.dev.boards.relations#{}',
     exception_handler=azure_devops_exception_handler
 )
 
@@ -27,3 +34,12 @@ def load_work_commands(self, _):
 
         # query commands
         g.command('query', 'query_work_items', table_transformer=transform_work_item_query_result_table_output)
+
+    with self.command_group('boards work-item', command_type=relationsOps) as g:
+        # relation commands
+        g.command('relation list-type', 'get_relation_types_show',
+                  table_transformer=transform_work_item_relation_type_table_output)
+        g.command('relation add', 'add_relation', table_transformer=transform_work_item_relations)
+        g.command('relation remove', 'remove_relation', table_transformer=transform_work_item_relations,
+                  confirmation='Are you sure you want to remove this relation(s)?')
+        g.command('relation show', 'show_work_item', table_transformer=transform_work_item_relations)
