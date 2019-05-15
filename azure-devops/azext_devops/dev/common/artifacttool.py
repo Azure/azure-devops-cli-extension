f504e7cb8d43623b1da9c8e472cde22ad8af554a
Vinit Suri <visuri@microsoft.com>
2019-05-15 14:14:08 -0700

    camel case to underscore

--- azure-devops/azext_devops/dev/common/artifacttool.py
+++ azure-devops/azext_devops/dev/common/artifacttool.py
@@ -30,11 +30,11 @@ class ArtifactToolInvoker:
                 "--project", project, "--pipeline-id", run_id, "--artifact-name", artifact_name, "--path", path]
         return self.run_artifacttool(organization, args, "Uploading")
 
-    def download_universal(self, organization, feed, package_name, package_version, path, fileFilter):
+    def download_universal(self, organization, feed, package_name, package_version, path, file_filter):
         args = ["universal", "download", "--service", organization, "--patvar", ARTIFACTTOOL_PAT_ENVKEY,
                 "--feed", feed, "--package-name", package_name, "--package-version", package_version,
                 "--path", path,
-                "--filter", fileFilter]
+                "--filter", file_filter]
         return self.run_artifacttool(organization, args, "Downloading")
 
     def publish_universal(self, organization, feed, package_name, package_version, description, path):
