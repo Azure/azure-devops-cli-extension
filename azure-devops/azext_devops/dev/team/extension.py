98b51e02112ef8ebb75fa157f962731120bd7ea7
gauravsaralms <43203065+gauravsaralMs@users.noreply.github.com>
2019-05-17 13:52:31 +0530

    Fix param names in extension commands (#598)

--- azure-devops/azext_devops/dev/team/extension.py
+++ azure-devops/azext_devops/dev/team/extension.py
@@ -112,63 +112,63 @@ def list_extensions(include_built_in='true', include_disabled='true', organizati
     return extensions
 
 
-def get_extension(publisher_id, extension_id, organization=None, detect=None):
+def get_extension(publisher_name, extension_name, organization=None, detect=None):
     """ Get detail of single extension
     """
     organization = resolve_instance(detect=detect, organization=organization)
     extension_client = get_extension_client(organization)
-    return extension_client.get_installed_extension_by_name(publisher_name=publisher_id,
-                                                            extension_name=extension_id)
+    return extension_client.get_installed_extension_by_name(publisher_name=publisher_name,
+                                                            extension_name=extension_name)
 
 
-def install_extension(publisher_id, extension_id, organization=None, detect=None):
+def install_extension(publisher_name, extension_name, organization=None, detect=None):
     """ Install an extension
     """
     organization = resolve_instance(detect=detect, organization=organization)
     extension_client = get_extension_client(organization)
-    return extension_client.install_extension_by_name(publisher_name=publisher_id,
-                                                      extension_name=extension_id)
+    return extension_client.install_extension_by_name(publisher_name=publisher_name,
+                                                      extension_name=extension_name)
 
 
-def uninstall_extension(publisher_id, extension_id, organization=None, detect=None):
+def uninstall_extension(publisher_name, extension_name, organization=None, detect=None):
     """ Uninstall an extension
     """
     organization = resolve_instance(detect=detect, organization=organization)
     extension_client = get_extension_client(organization)
-    return extension_client.uninstall_extension_by_name(publisher_name=publisher_id,
-                                                        extension_name=extension_id)
+    return extension_client.uninstall_extension_by_name(publisher_name=publisher_name,
+                                                        extension_name=extension_name)
 
 
-def enable_extension(publisher_id, extension_id, organization=None, detect=None):
+def enable_extension(publisher_name, extension_name, organization=None, detect=None):
     """ Enable an extension
     """
     return _update_extension_state(disable=False,
                                    enable=True,
-                                   publisher_id=publisher_id,
-                                   extension_id=extension_id,
+                                   publisher_name=publisher_name,
+                                   extension_name=extension_name,
                                    organization=organization,
                                    detect=detect)
 
 
-def disable_extension(publisher_id, extension_id, organization=None, detect=None):
+def disable_extension(publisher_name, extension_name, organization=None, detect=None):
     """ Disable an extension
     """
     return _update_extension_state(disable=True,
                                    enable=False,
-                                   publisher_id=publisher_id,
-                                   extension_id=extension_id,
+                                   publisher_name=publisher_name,
+                                   extension_name=extension_name,
                                    organization=organization,
                                    detect=detect)
 
 
 def _update_extension_state(disable, enable,
-                            publisher_id, extension_id,
+                            publisher_name, extension_name,
                             organization=None, detect=None):
     organization = resolve_instance(detect=detect, organization=organization)
     extension_client = get_extension_client(organization)
     current_extension = extension_client.get_installed_extension_by_name(
-        publisher_name=publisher_id,
-        extension_name=extension_id)
+        publisher_name=publisher_name,
+        extension_name=extension_name)
 
     state_from_service = str(current_extension.install_state.flags)
     logger.info('state received from service')
