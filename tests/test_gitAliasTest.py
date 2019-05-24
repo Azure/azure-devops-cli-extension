180533f252df46e04c28f7fbed70ebd076a62473
Atul Bagga <atbagga@microsoft.com>
2019-05-24 18:56:17 +0530

    Add tests

---
+++ tests/test_gitAliasTest.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+import subprocess
+from azure.cli.testsdk import ScenarioTest
+
+class TestGitAliasing(ScenarioTest):
+    def test_git_aliases_pr_repo(self):
+        self.cmd('az devops configure --use-git-aliases')
+        repo_help = subprocess.check_output('git repo -h', shell=True)
+        pr_help = subprocess.check_output('git pr -h', shell=True)
