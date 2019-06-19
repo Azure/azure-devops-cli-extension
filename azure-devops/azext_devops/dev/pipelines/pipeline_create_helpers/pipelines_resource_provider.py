c696f8f2eff6aa59aba4e3c71eb6604737cc4c3e
Atul Bagga <atbagga@microsoft.com>
2019-06-19 12:58:22 +0530

    Fix polling spinner on windows (#672)

--- azure-devops/azext_devops/dev/pipelines/pipeline_create_helpers/pipelines_resource_provider.py
+++ azure-devops/azext_devops/dev/pipelines/pipeline_create_helpers/pipelines_resource_provider.py
@@ -156,9 +156,9 @@ def get_kubernetes_namespace(organization, project, cluster, subscription_id, su
 
 def poll_connection_ready(organization, project, connection_id):
     import colorama
+    colorama.init()
     import humanfriendly
     import time
-    colorama.init()
     with humanfriendly.Spinner(label="Checking resource readiness") as spinner:
         se_client = get_service_endpoint_client(organization)
         while True:
@@ -167,6 +167,7 @@ def poll_connection_ready(organization, project, connection_id):
             service_endpoint = se_client.get_service_endpoint_details(project, connection_id)
             if service_endpoint.is_ready:
                 break
+    colorama.deinit()
 
 
 def get_se_kubernetes_namespace_request_obj(subscription_id, subscription_name, cluster_id, cluster_name, fqdn,
