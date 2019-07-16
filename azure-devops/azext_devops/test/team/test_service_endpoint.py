139cbea39689f74e1826780a8b3e84d53e469f2b
Gaurav Saral <gsaral@microsoft.com>
2019-07-16 12:59:58 +0530

    UTs

--- azure-devops/azext_devops/test/team/test_service_endpoint.py
+++ azure-devops/azext_devops/test/team/test_service_endpoint.py
@@ -17,6 +17,8 @@ from knack.prompting import NoTTYException
 from azext_devops.dev.team.service_endpoint import (list_service_endpoints,
                                                     show_service_endpoint,
                                                     create_service_endpoint,
+                                                    create_github_service_endpoint,
+                                                    create_azurerm_service_endpoint,
                                                     delete_service_endpoint)
 from azext_devops.dev.team.const import (SERVICE_ENDPOINT_AUTHORIZATION_PERSONAL_ACCESS_TOKEN,
                                         SERVICE_ENDPOINT_TYPE_GITHUB,
@@ -74,62 +76,47 @@ class TestServiceEndpointMethods(AuthenticatedTests):
         #assert 
         self.mock_delete_SE(self._TEST_PROJECT_NAME, randomId, 'false')
 
-    # def test_create_service_endpoint_unsupported(self):
-    #     try:
-    #         create_service_endpoint(service_endpoint_type = SERVICE_ENDPOINT_TYPE_GITHUB, authorization_scheme = SERVICE_ENDPOINT_AUTHORIZATION_SERVICE_PRINCIPAL, name = '')
-    #         self.fail()
-    #     except CLIError as ex:
-    #         pass
+    def test_create_service_endpoint_github(self):
+        import os
+        os.environ['AZURE_DEVOPS_EXT_GITHUB_PAT'] = 'fakeToken'
+        response = create_github_service_endpoint(name = '',
+                                                  organization = self._TEST_DEVOPS_ORGANIZATION, 
+                                                  project = self._TEST_PROJECT_NAME)
+        del os.environ['AZURE_DEVOPS_EXT_GITHUB_PAT']
 
-    # def test_create_service_endpoint_github(self):
-    #     import os
-    #     os.environ['AZURE_DEVOPS_EXT_GITHUB_PAT'] = 'fakeToken'
-    #     response = create_service_endpoint(service_endpoint_type = SERVICE_ENDPOINT_TYPE_GITHUB, 
-    #                                        authorization_scheme = SERVICE_ENDPOINT_AUTHORIZATION_PERSONAL_ACCESS_TOKEN, 
-    #                                        name = '',
-    #                                        organization = self._TEST_DEVOPS_ORGANIZATION,
-    #                                        project = self._TEST_PROJECT_NAME)
-    #     del os.environ['AZURE_DEVOPS_EXT_GITHUB_PAT']
+        #assert
+        # not doing extensive comparision because object creation code does not have much logic
+        self.mock_create_SE.assert_called_once()
 
-    #     #assert
-    #     # not doing extensive comparision because object creation code does not have much logic
-    #     self.mock_create_SE.assert_called_once()
+    def test_create_service_endpoint_azure_rm(self):
+        import os
+        os.environ['AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY'] = 'fakeKey'
+        response = create_azurerm_service_endpoint(name = '',
+                                                   organization = self._TEST_DEVOPS_ORGANIZATION,
+                                                   project = self._TEST_PROJECT_NAME)
+        del os.environ['AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY']
 
-    # def test_create_service_endpoint_azure_rm(self):
-    #     import os
-    #     os.environ['AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY'] = 'fakeKey'
-    #     response = create_service_endpoint(service_endpoint_type = SERVICE_ENDPOINT_TYPE_AZURE_RM, 
-    #                                        authorization_scheme = SERVICE_ENDPOINT_AUTHORIZATION_SERVICE_PRINCIPAL, 
-    #                                        name = '',
-    #                                        organization = self._TEST_DEVOPS_ORGANIZATION,
-    #                                        project = self._TEST_PROJECT_NAME)
-    #     del os.environ['AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY']
+        #assert
+        # not doing extensive comparision because object creation code does not have much logic
+        self.mock_create_SE.assert_called_once()
 
-    #     #assert
-    #     # not doing extensive comparision because object creation code does not have much logic
-    #     self.mock_create_SE.assert_called_once()
+    def test_create_service_endpoint_ttyi_exception_github(self):
+        try:
+            response = create_service_endpoint(name = '',
+                                               organization = self._TEST_DEVOPS_ORGANIZATION,
+                                               project = self._TEST_PROJECT_NAME)
+            self.fail('exception was expected')
+        except NoTTYException as ex:
+            self.assertEqual(str(ex), 'Please pass GitHub access token in AZURE_DEVOPS_EXT_GITHUB_PAT environment variable in non-interactive mode.')
 
-    # def test_create_service_endpoint_ttyi_exception_github(self):
-    #     try:
-    #         response = create_service_endpoint(service_endpoint_type = SERVICE_ENDPOINT_TYPE_GITHUB,
-    #                                            authorization_scheme = SERVICE_ENDPOINT_AUTHORIZATION_PERSONAL_ACCESS_TOKEN,
-    #                                            name = '',
-    #                                            organization = self._TEST_DEVOPS_ORGANIZATION,
-    #                                            project = self._TEST_PROJECT_NAME)
-    #         self.fail('exception was expected')
-    #     except NoTTYException as ex:
-    #         self.assertEqual(str(ex), 'Please pass GitHub access token in AZURE_DEVOPS_EXT_GITHUB_PAT environment variable in non-interactive mode.')
-
-    # def test_create_service_endpoint_ttyi_exception_azure_se(self):
-    #     try:
-    #         response = create_service_endpoint(service_endpoint_type = SERVICE_ENDPOINT_TYPE_AZURE_RM, 
-    #                                         authorization_scheme = SERVICE_ENDPOINT_AUTHORIZATION_SERVICE_PRINCIPAL, 
-    #                                         name = '',
-    #                                         organization = self._TEST_DEVOPS_ORGANIZATION,
-    #                                         project = self._TEST_PROJECT_NAME)
-    #         self.fail('exception was expected')
-    #     except NoTTYException as ex:
-    #         self.assertEqual(str(ex), 'Please specify azure service principal key in AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY environment variable in non-interactive mode.')
+    def test_create_service_endpoint_ttyi_exception_azure_se(self):
+        try:
+            response = create_service_endpoint(name = '',
+                                               organization = self._TEST_DEVOPS_ORGANIZATION,
+                                               project = self._TEST_PROJECT_NAME)
+            self.fail('exception was expected')
+        except NoTTYException as ex:
+            self.assertEqual(str(ex), 'Please specify azure service principal key in AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY environment variable in non-interactive mode.')
 
 if __name__ == '__main__':
     unittest.main()
