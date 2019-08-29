21c4a9a273612dfce7f5878eb24d71b673ccaf90
gauravsaralms <43203065+gauravsaralMs@users.noreply.github.com>
2019-08-29 12:06:45 +0530

    handle AzureDevOpsAuthenticationError and raise CLIError for the same (#769)

--- azure-devops/azext_devops/dev/common/exception_handler.py
+++ azure-devops/azext_devops/dev/common/exception_handler.py
@@ -12,11 +12,16 @@ logger = get_logger(__name__)
 def azure_devops_exception_handler(ex):
     # we are taking dependency on string here because taking dependency on
     # Azure DevOps Client will increase load time for every command
-    if type(ex).__name__ == 'AzureDevOpsServiceError':
+    exceptionTypeName = type(ex).__name__
+    if exceptionTypeName == 'AzureDevOpsServiceError':
         logger.debug('handling vsts service error')
         raise CLIError(ex)
 
-    if type(ex).__name__ == 'ValueError':
+    if exceptionTypeName == 'AzureDevOpsAuthenticationError':
+        logger.debug('handling vsts auth error')
+        raise CLIError(ex)
+
+    if exceptionTypeName == 'ValueError':
         logger.debug('handling value error')
         raise CLIError(ex)
 
