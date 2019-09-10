a0285aa5e620f7b5bc50a38696b34d76f7c36a51
Atul Bagga <atbagga@microsoft.com>
2019-09-10 15:56:33 +0530

    minor issue

--- azure-devops/azext_devops/dev/pipelines/build_definition.py
+++ azure-devops/azext_devops/dev/pipelines/build_definition.py
@@ -91,11 +91,13 @@ def _open_definition(definition, organization):
 
 
 def fix_path_for_api(path):
-    if path.startswith('/'):
-        path = path[1:]
-    if not path.startswith('\\'):
-        path = '\\' + path
+    if path:
+        if path.startswith('/'):
+            path = path[1:]
+        if not path.startswith('\\'):
+            path = '\\' + path
     return path
+
 
 def get_definition_id_from_name(name, client, project, path=None):
     # Path with no preceeding '\' is not correctly interpreted so hack to add it.
