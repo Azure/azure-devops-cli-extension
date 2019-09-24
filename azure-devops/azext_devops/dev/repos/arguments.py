eeb2b174b4157c6ddc2a1f6cde941e4aaebc91b8
Atul Bagga <atbagga@microsoft.com>
2019-09-24 15:43:21 +0530

    bug bash fixes ++

--- azure-devops/azext_devops/dev/repos/arguments.py
+++ azure-devops/azext_devops/dev/repos/arguments.py
@@ -66,7 +66,7 @@ def load_code_arguments(self, _):
         context.argument('use_squash_merge', arg_type=get_three_state_flag(),
                          help='Whether to squash merge always. '
                          'This option does not work for allowing other merge types.',
-                         deprecate_info=context.deprecate(redirect="--allow-squash"))
+                         deprecate_info=context.deprecate(redirect='--allow-squash', target='--use-squash-merge'))
         context.argument('allow_squash', arg_type=get_three_state_flag(),
                          help='Squash merge - Creates a linear history by condensing the source branch commits '
                          'into a single new commit on the target branch.')
