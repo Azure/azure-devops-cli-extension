40714e5e140323bb66568a005e8cf5ab72658d71
Atul Bagga <atbagga@microsoft.com>
2019-10-01 14:25:39 +0530

    Increment version for next release

--- azure-devops/azext_devops/version.py
+++ azure-devops/azext_devops/version.py
@@ -3,4 +3,4 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-VERSION = "0.13.0"
+VERSION = "0.14.0"
