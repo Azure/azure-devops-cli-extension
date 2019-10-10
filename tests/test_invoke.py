c5d8e75cb9e1f9f56b96c0e6cac752042a64e5f9
Gaurav Saral <gsaral@microsoft.com>
2019-10-10 13:54:01 +0530

    fixed invoke test

--- tests/test_invoke.py
+++ tests/test_invoke.py
@@ -8,7 +8,7 @@ import unittest
 from azure_devtools.scenario_tests import AllowLargeResponse
 from .utilities.helper import DevopsScenarioTest, disable_telemetry, set_authentication, get_test_org_from_env_variable
 
-DEVOPS_CLI_TEST_ORGANIZATION = get_test_org_from_env_variable() or 'https://dev.azure.com/gsaralprivate'
+DEVOPS_CLI_TEST_ORGANIZATION = get_test_org_from_env_variable() or 'https://dev.azure.com/azuredevopsclitest'
 
 class TestInvoke(DevopsScenarioTest):
     @AllowLargeResponse(size_kb=3072)
@@ -17,6 +17,6 @@ class TestInvoke(DevopsScenarioTest):
     def test_invoke_command_basic(self):
         self.cmd('az devops configure --defaults organization=' +  DEVOPS_CLI_TEST_ORGANIZATION)
 
-        projects_classic = self.cmd('az devops project list -o json --detect false').get_output_in_json()
-        projects = self.cmd('az devops invoke --area core --resource projects -o json --detect false').get_output_in_json()['value']
+        projects_classic = self.cmd('az devops project list -o json').get_output_in_json()
+        projects = self.cmd('az devops invoke --area core --resource projects -o json').get_output_in_json()['value']
         assert len(projects) == len(projects_classic)
