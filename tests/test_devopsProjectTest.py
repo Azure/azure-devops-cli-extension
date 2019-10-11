3ef9b06882c37d1d038a21a2e8818a45a09a2a28
Gaurav Saral <gsaral@microsoft.com>
2019-10-11 09:25:53 +0530

    maybe this fixes it

--- tests/test_devopsProjectTest.py
+++ tests/test_devopsProjectTest.py
@@ -36,13 +36,9 @@ class DevopsProjectTests(DevopsScenarioTest):
             
             list_project_command = 'az devops project list --output json'
             list_project_output = self.cmd(list_project_command).get_output_in_json()
-            print("Printing list_project_output")
-            print(json.dumps(list_project_output.value))
             verified_project_list = False
             assert len(list_project_output.value) > 1
             for project in list_project_output.value:
-                print("Printing object")
-                print(json.dumps(project))
                 if (project["id"] == created_project_id):
                     verified_project_list = True
             assert verified_project_list == True
@@ -60,7 +56,7 @@ class DevopsProjectTests(DevopsScenarioTest):
             #Verify Deletion
             list_project_command = 'az devops project list --output json'
             list_project_output_after_delete = self.cmd(list_project_command).get_output_in_json()
-            for project in list_project_output_after_delete:
+            for project in list_project_output_after_delete.value:
                 if (project["id"] == created_project_id):
                     assert 0
             
