8e6d3a3056026cf033d5c010196f9c53d8e7fdae
Salvador Melendez-Delgado <sameln@microsoft.com>
2019-10-23 14:23:47 -0700

    Adding scope parameter

--- azure-devops/azext_devops/dev/artifacts/arguments.py
+++ azure-devops/azext_devops/dev/artifacts/arguments.py
@@ -3,10 +3,14 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from knack.arguments import enum_choice_list
+
+_SCOPE_VALUES = ['project', 'organization']
 
 def load_package_arguments(self, _):
     with self.argument_context('artifacts universal') as context:
         context.argument('name', options_list=('--name', '-n'))
         context.argument('version', options_list=('--version', '-v'))
+        context.argument('scope', **enum_choice_list(_SCOPE_VALUES))
     with self.argument_context('artifacts universal publish') as context:
         context.argument('description', options_list=('--description', '-d'))
--- azure-devops/azext_devops/dev/artifacts/universal.py
+++ azure-devops/azext_devops/dev/artifacts/universal.py
@@ -5,7 +5,8 @@
 
 import colorama
 from knack.log import get_logger
-from azext_devops.dev.common.services import resolve_instance_and_project
+from knack.util import CLIError
+from azext_devops.dev.common.services import resolve_instance, resolve_instance_and_project
 from azext_devops.dev.common.artifacttool import ArtifactToolInvoker
 from azext_devops.dev.common.artifacttool_updater import ArtifactToolUpdater
 from azext_devops.dev.common.external_tool import ProgressReportingExternalToolInvoker
@@ -13,8 +14,10 @@ from azext_devops.dev.common.external_tool import ProgressReportingExternalToolI
 logger = get_logger(__name__)
 
 
-def publish_package(feed, name, version, path, description=None, organization=None, project=None, detect=None):
+def publish_package(feed, name, version, path, description=None, scope='organization', organization=None, project=None, detect=None):
     """Publish a package to a feed.
+    :param scope: Scope of the feed: 'project' if the feed was created in a project, and 'organization' otherwise.
+    :type scope: str
     :param feed: Name or ID of the feed.
     :type feed: str
     :param name: Name of the package, e.g. 'foo-package'.
@@ -28,18 +31,27 @@ def publish_package(feed, name, version, path, description=None, organization=No
     """
     colorama.init()   # Needed for humanfriendly spinner to display correctly
 
-    organization, project = resolve_instance_and_project(
-        detect=detect,
-        organization=organization,
-        project=project,
-        project_required=False)
+    if scope == 'project':
+        organization, project = resolve_instance_and_project(
+            detect=detect,
+            organization=organization,
+            project=project)
+    else:
+        if project is not None:
+            raise CLIError('--scope \'project\' is required when specifying a project in --project')
+
+        organization = resolve_instance(
+            detect=detect,
+            organization=organization)
 
     artifact_tool = ArtifactToolInvoker(ProgressReportingExternalToolInvoker(), ArtifactToolUpdater())
     return artifact_tool.publish_universal(organization, project, feed, name, version, description, path)
 
 
-def download_package(feed, name, version, path, file_filter=None, organization=None, project=None, detect=None):
+def download_package(feed, name, version, path, file_filter=None, scope='organization', organization=None, project=None, detect=None):
     """Download a package.
+    :param scope: Scope of the feed: 'project' if the feed was created in a project, and 'organization' otherwise.
+    :type scope: str
     :param feed: Name or ID of the feed.
     :type feed: str
     :param name: Name of the package, e.g. 'foo-package'.
@@ -53,11 +65,18 @@ def download_package(feed, name, version, path, file_filter=None, organization=N
     """
     colorama.init()  # Needed for humanfriendly spinner to display correctly
 
-    organization, project = resolve_instance_and_project(
-        detect=detect,
-        organization=organization,
-        project=project,
-        project_required=False)
+    if scope == 'project':
+        organization, project = resolve_instance_and_project(
+            detect=detect,
+            organization=organization,
+            project=project)
+    else:
+        if project is not None:
+            raise CLIError('--scope \'project\' is required when specifying a project in --project')
+
+        organization = resolve_instance(
+            detect=detect,
+            organization=organization)
 
     artifact_tool = ArtifactToolInvoker(ProgressReportingExternalToolInvoker(), ArtifactToolUpdater())
     return artifact_tool.download_universal(organization, project, feed, name, version, path, file_filter)
