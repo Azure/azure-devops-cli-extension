9f56c6f32ac72bd3700b1ef2d39cd483b2532a28
Peter Spillman <pspill@microsoft.com>
2019-10-31 13:28:24 -0400

    add TestUniversalPackages.*_with_project tests

--- azure-devops/azext_devops/test/artifacts/test_univeral.py
+++ azure-devops/azext_devops/test/artifacts/test_univeral.py
@@ -22,7 +22,10 @@ from azext_devops.dev.common.services import clear_connection_cache
 
 class TestUniversalPackages(unittest.TestCase):
 
+    _CONST_ORGANIZATION_SCOPE = 'organization'
+    _CONST_PROJECT_SCOPE = 'project'
     _TEST_DEVOPS_ORGANIZATION = 'https://someorg.visualstudio.com'
+    _TEST_DEVOPS_PROJECT = 'my-test-project'
     _TEST_PAT_TOKEN = 'somepattoken'
     _TEST_FEED_NAME = 'my-test-feed'
     _TEST_PACKAGE_NAME = 'my-test-package'
@@ -57,15 +60,40 @@ class TestUniversalPackages(unittest.TestCase):
         # assert
         self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
             [
-                'universal', 'publish', 
-                '--service', self._TEST_DEVOPS_ORGANIZATION, 
-                 '--patvar', ARTIFACTTOOL_PAT_ENVKEY, 
-                 '--feed', self._TEST_FEED_NAME, 
-                 '--package-name', self._TEST_PACKAGE_NAME, 
-                 '--package-version', self._TEST_PACKAGE_VERSION, 
-                 '--path', self._TEST_PATH, 
+                'universal', 'publish',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                 '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                 '--feed', self._TEST_FEED_NAME,
+                 '--package-name', self._TEST_PACKAGE_NAME,
+                 '--package-version', self._TEST_PACKAGE_VERSION,
+                 '--path', self._TEST_PATH,
                  '--description', self._TEST_PACKAGE_DESCRIPTION
-            ], 
+            ],
+            'Publishing')
+
+    def test_publish_package_with_project(self):
+        response = publish_package(feed = self._TEST_FEED_NAME,
+            name = self._TEST_PACKAGE_NAME,
+            version = self._TEST_PACKAGE_VERSION,
+            description = self._TEST_PACKAGE_DESCRIPTION,
+            path = self._TEST_PATH,
+            organization = self._TEST_DEVOPS_ORGANIZATION,
+            project = self._TEST_DEVOPS_PROJECT,
+            scope = self._CONST_PROJECT_SCOPE)
+
+        # assert
+        self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
+            [
+                'universal', 'publish',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                 '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                 '--feed', self._TEST_FEED_NAME,
+                 '--package-name', self._TEST_PACKAGE_NAME,
+                 '--package-version', self._TEST_PACKAGE_VERSION,
+                 '--path', self._TEST_PATH,
+                 '--project', self._TEST_DEVOPS_PROJECT,
+                 '--description', self._TEST_PACKAGE_DESCRIPTION,
+            ],
             'Publishing')
         
     def test_download_package(self):
@@ -79,14 +107,39 @@ class TestUniversalPackages(unittest.TestCase):
         # assert
         self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
             [
-                'universal', 'download', 
-                '--service', self._TEST_DEVOPS_ORGANIZATION, 
-                    '--patvar', ARTIFACTTOOL_PAT_ENVKEY, 
-                    '--feed', self._TEST_FEED_NAME, 
-                    '--package-name', self._TEST_PACKAGE_NAME, 
-                    '--package-version', self._TEST_PACKAGE_VERSION, 
-                    '--path', self._TEST_PATH, 
+                'universal', 'download',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                    '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                    '--feed', self._TEST_FEED_NAME,
+                    '--package-name', self._TEST_PACKAGE_NAME,
+                    '--package-version', self._TEST_PACKAGE_VERSION,
+                    '--path', self._TEST_PATH,
                     '--filter', self._TEST_FILTER,
-            ], 
+            ],
+            'Downloading')
+
+    def test_download_package_with_project(self):
+        response = download_package(feed = self._TEST_FEED_NAME,
+            name = self._TEST_PACKAGE_NAME,
+            version = self._TEST_PACKAGE_VERSION,
+            path = self._TEST_PATH,
+            file_filter= self._TEST_FILTER,
+            organization = self._TEST_DEVOPS_ORGANIZATION,
+            project = self._TEST_DEVOPS_PROJECT,
+            scope = self._CONST_PROJECT_SCOPE)
+
+        # assert
+        self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
+            [
+                'universal', 'download',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                    '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                    '--feed', self._TEST_FEED_NAME,
+                    '--package-name', self._TEST_PACKAGE_NAME,
+                    '--package-version', self._TEST_PACKAGE_VERSION,
+                    '--path', self._TEST_PATH,
+                    '--project', self._TEST_DEVOPS_PROJECT,
+                    '--filter', self._TEST_FILTER,
+            ],
             'Downloading')
                 
