fc1d5cd32f76a86130818afeea1ab113656d55f1
Peter Spillman <pspill@microsoft.com>
2019-11-04 11:22:52 -0500

    Re-adding optional project parameter to artifacts universal commands + unit tests (#862)
    * Adding project parameter to UPack commands
    
    * Fix styling issue
    
    * Adding scope parameter
    
    * Pylint corrections
    
    * Fixing previously missed Pylint issues
    
    * Reverting unintentional change to vscode settings file and small fix in error message
    
    * add TestUniversalPackages.*_with_project tests
    
    * TestUniversalPackages two more unit tests

--- azure-devops/azext_devops/dev/artifacts/arguments.py
+++ azure-devops/azext_devops/dev/artifacts/arguments.py
@@ -4,9 +4,16 @@
 # --------------------------------------------------------------------------------------------
 
 
+from knack.arguments import enum_choice_list
+
+
+_SCOPE_VALUES = ['project', 'organization']
+
+
 def load_package_arguments(self, _):
     with self.argument_context('artifacts universal') as context:
         context.argument('name', options_list=('--name', '-n'))
         context.argument('version', options_list=('--version', '-v'))
+        context.argument('scope', **enum_choice_list(_SCOPE_VALUES))
     with self.argument_context('artifacts universal publish') as context:
         context.argument('description', options_list=('--description', '-d'))
--- azure-devops/azext_devops/test/artifacts/test_univeral.py
+++ azure-devops/azext_devops/test/artifacts/test_univeral.py
@@ -17,12 +17,16 @@ from azext_devops.dev.artifacts.universal import (publish_package,
 from azext_devops.dev.common.artifacttool import ArtifactToolInvoker
 from azext_devops.dev.common.const import ARTIFACTTOOL_PAT_ENVKEY
 from azext_devops.dev.common.services import clear_connection_cache
+from knack.util import CLIError
 
 
 
 class TestUniversalPackages(unittest.TestCase):
 
+    _CONST_ORGANIZATION_SCOPE = 'organization'
+    _CONST_PROJECT_SCOPE = 'project'
     _TEST_DEVOPS_ORGANIZATION = 'https://someorg.visualstudio.com'
+    _TEST_DEVOPS_PROJECT = 'my-test-project'
     _TEST_PAT_TOKEN = 'somepattoken'
     _TEST_FEED_NAME = 'my-test-feed'
     _TEST_PACKAGE_NAME = 'my-test-package'
@@ -57,16 +61,52 @@ class TestUniversalPackages(unittest.TestCase):
         # assert
         self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
             [
-                'universal', 'publish', 
-                '--service', self._TEST_DEVOPS_ORGANIZATION, 
-                 '--patvar', ARTIFACTTOOL_PAT_ENVKEY, 
-                 '--feed', self._TEST_FEED_NAME, 
-                 '--package-name', self._TEST_PACKAGE_NAME, 
-                 '--package-version', self._TEST_PACKAGE_VERSION, 
-                 '--path', self._TEST_PATH, 
+                'universal', 'publish',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                 '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                 '--feed', self._TEST_FEED_NAME,
+                 '--package-name', self._TEST_PACKAGE_NAME,
+                 '--package-version', self._TEST_PACKAGE_VERSION,
+                 '--path', self._TEST_PATH,
                  '--description', self._TEST_PACKAGE_DESCRIPTION
-            ], 
+            ],
             'Publishing')
+
+    def test_publish_package_with_project(self):
+        response = publish_package(feed = self._TEST_FEED_NAME,
+            name = self._TEST_PACKAGE_NAME,
+            version = self._TEST_PACKAGE_VERSION,
+            description = self._TEST_PACKAGE_DESCRIPTION,
+            path = self._TEST_PATH,
+            organization = self._TEST_DEVOPS_ORGANIZATION,
+            project = self._TEST_DEVOPS_PROJECT,
+            scope = self._CONST_PROJECT_SCOPE)
+
+        # assert
+        self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
+            [
+                'universal', 'publish',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                 '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                 '--feed', self._TEST_FEED_NAME,
+                 '--package-name', self._TEST_PACKAGE_NAME,
+                 '--package-version', self._TEST_PACKAGE_VERSION,
+                 '--path', self._TEST_PATH,
+                 '--project', self._TEST_DEVOPS_PROJECT,
+                 '--description', self._TEST_PACKAGE_DESCRIPTION,
+            ],
+            'Publishing')
+
+    def test_publish_package_when_project_without_scope_then_exception(self):
+        with self.assertRaises(CLIError) as exc:
+            response = publish_package(feed = self._TEST_FEED_NAME,
+                name = self._TEST_PACKAGE_NAME,
+                version = self._TEST_PACKAGE_VERSION,
+                description = self._TEST_PACKAGE_DESCRIPTION,
+                path = self._TEST_PATH,
+                organization = self._TEST_DEVOPS_ORGANIZATION,
+                project = self._TEST_DEVOPS_PROJECT)
+        self.assertIn('--scope \'project\' is required when specifying a value in --project', str(exc.exception))
         
     def test_download_package(self):
         response = download_package(feed = self._TEST_FEED_NAME,
@@ -79,14 +119,50 @@ class TestUniversalPackages(unittest.TestCase):
         # assert
         self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
             [
-                'universal', 'download', 
-                '--service', self._TEST_DEVOPS_ORGANIZATION, 
-                    '--patvar', ARTIFACTTOOL_PAT_ENVKEY, 
-                    '--feed', self._TEST_FEED_NAME, 
-                    '--package-name', self._TEST_PACKAGE_NAME, 
-                    '--package-version', self._TEST_PACKAGE_VERSION, 
-                    '--path', self._TEST_PATH, 
+                'universal', 'download',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                    '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                    '--feed', self._TEST_FEED_NAME,
+                    '--package-name', self._TEST_PACKAGE_NAME,
+                    '--package-version', self._TEST_PACKAGE_VERSION,
+                    '--path', self._TEST_PATH,
                     '--filter', self._TEST_FILTER,
-            ], 
+            ],
             'Downloading')
+
+    def test_download_package_with_project(self):
+        response = download_package(feed = self._TEST_FEED_NAME,
+            name = self._TEST_PACKAGE_NAME,
+            version = self._TEST_PACKAGE_VERSION,
+            path = self._TEST_PATH,
+            file_filter= self._TEST_FILTER,
+            organization = self._TEST_DEVOPS_ORGANIZATION,
+            project = self._TEST_DEVOPS_PROJECT,
+            scope = self._CONST_PROJECT_SCOPE)
+
+        # assert
+        self.mock_run_artifacttool.assert_called_with(self._TEST_DEVOPS_ORGANIZATION,
+            [
+                'universal', 'download',
+                '--service', self._TEST_DEVOPS_ORGANIZATION,
+                    '--patvar', ARTIFACTTOOL_PAT_ENVKEY,
+                    '--feed', self._TEST_FEED_NAME,
+                    '--package-name', self._TEST_PACKAGE_NAME,
+                    '--package-version', self._TEST_PACKAGE_VERSION,
+                    '--path', self._TEST_PATH,
+                    '--project', self._TEST_DEVOPS_PROJECT,
+                    '--filter', self._TEST_FILTER,
+            ],
+            'Downloading')
+
+    def test_download_package_when_project_without_scope_then_exception(self):
+        with self.assertRaises(CLIError) as exc:
+            response = download_package(feed = self._TEST_FEED_NAME,
+                name = self._TEST_PACKAGE_NAME,
+                version = self._TEST_PACKAGE_VERSION,
+                path = self._TEST_PATH,
+                file_filter= self._TEST_FILTER,
+                organization = self._TEST_DEVOPS_ORGANIZATION,
+                project = self._TEST_DEVOPS_PROJECT)
+        self.assertIn('--scope \'project\' is required when specifying a value in --project', str(exc.exception))
                 
