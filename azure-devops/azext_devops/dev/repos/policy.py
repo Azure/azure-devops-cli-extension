8cca7cd238ac96b0835a8525a721e2671fb673e4
Mitesh Shah <mitsha@microsoft.com>
2020-04-01 12:12:58 +0530

    Fixing update polciy as well

--- azure-devops/azext_devops/dev/repos/policy.py
+++ azure-devops/azext_devops/dev/repos/policy.py
@@ -602,7 +602,7 @@ def update_policy_case_enforcement(policy_id,
         None,
         blocking if blocking is not None else current_policy.is_blocking,
         enabled if enabled is not None else current_policy.is_enabled,
-        '40e92b44-2fe1-4dd6-b3d8-74a9c21d0c6e',
+        '7ed39669-655c-494e-b4a0-a08b4da0fcce',
         ['enforceConsistentCase'],
         ['true']
     )
