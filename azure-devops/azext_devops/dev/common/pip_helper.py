61bec2b812d0e0475065f2dca3e43143459b363c
Sarthak Munjal <t-samun@microsoft.com>
2020-06-04 14:06:17 +0530

    Removing pip_helper dependency from cli

--- azure-devops/azext_devops/dev/common/pip_helper.py
+++ azure-devops/azext_devops/dev/common/pip_helper.py
@@ -3,10 +3,12 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import sys
+from subprocess import check_output, STDOUT, CalledProcessError
+
 from knack.log import get_logger
 from knack.util import CLIError
 
-from azure.cli.core.extension.operations import _run_pip
 from azure.cli.core.extension import get_extension_path
 
 logger = get_logger(__name__)
@@ -15,6 +17,18 @@ logger = get_logger(__name__)
 def install_keyring():
     _install_package('keyring~=17.1.1')
 
+def _run_pip(pip_exec_args):
+    cmd = [sys.executable, '-m', 'pip'] + pip_exec_args + ['-vv', '--disable-pip-version-check', '--no-cache-dir']
+    logger.debug('Running: %s', cmd)
+    try:
+        log_output = check_output(cmd, stderr=STDOUT, universal_newlines=True)
+        logger.debug(log_output)
+        returncode = 0
+    except CalledProcessError as e:
+        logger.debug(e.output)
+        logger.debug(e)
+        returncode = e.returncode
+    return returncode
 
 def _install_package(package_name):
     logger.debug('installing %s', package_name)
