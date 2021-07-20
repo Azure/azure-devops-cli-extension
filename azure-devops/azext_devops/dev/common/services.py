dd0cbffc622bc07c295abe421b21c53f4e9c17a4
Roshan-sy <roshan-sy@github.com>
2021-07-20 15:43:18 +0530

    run style chagnes

--- azure-devops/azext_devops/dev/common/services.py
+++ azure-devops/azext_devops/dev/common/services.py
@@ -355,12 +355,12 @@ def resolve_instance_project_and_repo(
         _raise_team_project_arg_error()
     if repo_required and repo is None:
         _raise_repo_requird_arg_error()
-        
-    if not (organization.startswith("https://dev.azure.com/") or organization.rstrip("/").endswith(".visualstudio.com")):
+ 
+    if not (organization.startswith("https://dev.azure.com/") 
+        or organization.rstrip("/").endswith(".visualstudio.com")):
         logger.warning("Support for Azure DevOps Server, is in preview and under development")
 
     return organization, project, repo
-
 
 def resolve_instance_and_project(detect, organization, project=None, project_required=True):
     organization, project, _ = resolve_instance_project_and_repo(
