6de84e9eafd093d353476383795c6f1cdc0ad2df
Roshan-sy <roshan-sy@github.com>
2021-08-03 11:58:11 +0530

    run style changes

--- azure-devops/azext_devops/dev/repos/ref.py
+++ azure-devops/azext_devops/dev/repos/ref.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 from knack.log import get_logger
+from knack.util import CLIError
 from azext_devops.devops_sdk.v5_0.git.models import GitRefUpdate
 from azext_devops.dev.common.git import resolve_git_refs
 from azext_devops.dev.common.services import (get_git_client,
@@ -72,11 +73,11 @@ def delete_ref(name, object_id=None, repository=None, organization=None, project
     client = get_git_client(organization)
 
     if object_id is None:
-        ref = client.get_refs(repository_id=repository,project=project,filter=name)
+        ref = client.get_refs(repository_id=repository, project=project, filter=name)
         if not ref or len(ref) > 1:
             logger.error('ref not found')
             raise CLIError("Failed to find object_id for ref " + name + ". Please provide object_id.")
-        
+
         object_id = ref[0].object_id
 
     ref_update = GitRefUpdate(name=resolve_git_refs(name),
