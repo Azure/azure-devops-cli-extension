6da64ec9e95225b64533eaa34696a3396a162562
Roshan-sy <roshan-sy@github.com>
2021-08-05 09:55:08 +0530

    command replaced by show_command

--- azure-devops/azext_devops/dev/pipelines/commands.py
+++ azure-devops/azext_devops/dev/pipelines/commands.py
@@ -108,7 +108,7 @@ def load_build_commands(self, _):
 
     with self.command_group('pipelines', command_type=pipelinesOps) as g:
         g.command('list', 'pipeline_list', table_transformer=transform_pipelines_table_output, is_preview=True)
-        g.command('show', 'pipeline_show', table_transformer=transform_pipeline_table_output, is_preview=True)
+        g.show_command('show', 'pipeline_show', table_transformer=transform_pipeline_table_output, is_preview=True)
         g.command('delete', 'pipeline_delete', confirmation='Are you sure you want to delete this pipeline?',
                   is_preview=True)
         g.command('run', 'pipeline_run', table_transformer=transform_pipeline_run_table_output, is_preview=True)
@@ -117,7 +117,7 @@ def load_build_commands(self, _):
         # basic build commands
         g.command('list', 'build_list', table_transformer=transform_builds_table_output)
         g.command('queue', 'build_queue', table_transformer=transform_build_table_output)
-        g.command('show', 'build_show', table_transformer=transform_build_table_output)
+        g.show_command('show', 'build_show', table_transformer=transform_build_table_output)
 
     with self.command_group('pipelines build tag', command_type=buildOps) as g:
         # basic build tag commands
@@ -128,18 +128,18 @@ def load_build_commands(self, _):
     with self.command_group('pipelines build definition', command_type=buildDefOps) as g:
         # basic build definition commands
         g.command('list', 'build_definition_list', table_transformer=transform_definitions_table_output)
-        g.command('show', 'build_definition_show', table_transformer=transform_definition_table_output)
+        g.show_command('show', 'build_definition_show', table_transformer=transform_definition_table_output)
 
     with self.command_group('pipelines release', command_type=releaseOps) as g:
         # basic release commands
         g.command('list', 'release_list', table_transformer=transform_releases_table_output)
         g.command('create', 'release_create', table_transformer=transform_release_table_output)
-        g.command('show', 'release_show', table_transformer=transform_release_table_output)
+        g.show_command('show', 'release_show', table_transformer=transform_release_table_output)
 
     with self.command_group('pipelines release definition', command_type=releaseDefinitionOps) as g:
         # basic release commands
         g.command('list', 'release_definition_list', table_transformer=transform_release_definitions_table_output)
-        g.command('show', 'release_definition_show', table_transformer=transform_release_definition_table_output)
+        g.show_command('show', 'release_definition_show', table_transformer=transform_release_definition_table_output)
 
     with self.command_group('pipelines runs artifact', command_type=pipelineRunArtifactsOps, is_preview=True) as g:
         g.command('download', 'run_artifact_download')
@@ -153,23 +153,23 @@ def load_build_commands(self, _):
 
     with self.command_group('pipelines runs', command_type=pipelinesRunOps, is_preview=True) as g:
         g.command('list', 'pipeline_run_list', table_transformer=transform_pipeline_runs_table_output)
-        g.command('show', 'pipeline_run_show', table_transformer=transform_pipeline_run_table_output)
+        g.show_command('show', 'pipeline_run_show', table_transformer=transform_pipeline_run_table_output)
 
     with self.command_group('pipelines pool', command_type=pipelineAgentPoolQueueOps, is_preview=True) as g:
         g.command('list', 'list_pools', table_transformer=transform_pipelines_pools_table_output)
-        g.command('show', 'show_pool', table_transformer=transform_pipelines_pool_table_output)
+        g.show_command('show', 'show_pool', table_transformer=transform_pipelines_pool_table_output)
 
     with self.command_group('pipelines agent', command_type=pipelineAgentPoolQueueOps, is_preview=True) as g:
         g.command('list', 'list_agents', table_transformer=transform_pipelines_agents_table_output)
-        g.command('show', 'show_agent', table_transformer=transform_pipelines_agent_table_output)
+        g.show_command('show', 'show_agent', table_transformer=transform_pipelines_agent_table_output)
 
     with self.command_group('pipelines queue', command_type=pipelineAgentPoolQueueOps, is_preview=True) as g:
         g.command('list', 'list_queues', table_transformer=transform_pipelines_queues_table_output)
-        g.command('show', 'show_queue', table_transformer=transform_pipelines_queue_table_output)
+        g.show_command('show', 'show_queue', table_transformer=transform_pipelines_queue_table_output)
 
     with self.command_group('pipelines variable-group', command_type=pipelineVariableGroupOps, is_preview=True) as g:
         g.command('create', 'variable_group_create', table_transformer=transform_pipelines_variable_group_table_output)
-        g.command('show', 'variable_group_show', table_transformer=transform_pipelines_variable_group_table_output)
+        g.show_command('show', 'variable_group_show', table_transformer=transform_pipelines_variable_group_table_output)
         g.command('list', 'variable_group_list', table_transformer=transform_pipelines_variable_groups_table_output)
         g.command('update', 'variable_group_update', table_transformer=transform_pipelines_variable_group_table_output)
         g.command('delete', 'variable_group_delete',
