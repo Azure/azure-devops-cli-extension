4042e5e3b367a74a9d1c0c44613312d485f14227
Thomas Moerkerken <1309462+moerketh@users.noreply.github.com>
2022-03-11 17:42:23 +0100

    fix formatting

--- tests/test_boardsWorkItemTagsTest.py
+++ tests/test_boardsWorkItemTagsTest.py
@@ -21,14 +21,14 @@ class BoardsWorkItemTagsTests(DevopsScenarioTest):
     def test_boards_work_item_tags_add_list_remove_tag(self):
         wi_name = 'samplebug'
         wi_test_project_name = 'WorkItemAddListRemoveTagTests'
-        wi_id =0
+        wi_id = 0
         wi_tag1 = "my tag"
         wi_tag2 = "tag2"
         self.cmd('az devops configure --defaults organization=' + DEVOPS_CLI_TEST_ORGANIZATION)
 
         try:
-            create_wi_command = 'az boards work-item create --project '+ wi_test_project_name +' --title ' + wi_name \
-                                + ' --type Bug  --detect false --output json'
+            create_wi_command = (f'az boards work-item create --project {wi_test_project_name} --title {wi_name} '
+                                ' --type Bug  --detect false --output json')
             wi_create = self.cmd(create_wi_command, checks=[
                 self.check('fields."System.AreaPath"', wi_test_project_name),
                 self.check('fields."System.WorkItemType"', 'Bug'),
@@ -81,6 +81,6 @@ class BoardsWorkItemTagsTests(DevopsScenarioTest):
 
             # verify if the work item is deleted or not
             with self.assertRaises(CLIError) as wi_except:
-                wi_show = self.cmd(show_wi_command).get_output_in_json()
+                self.cmd(show_wi_command).get_output_in_json()
             self.assertEqual(str(wi_except.exception), (f'TF401232: Work item {str(wi_id)} does not exist, '
                                                         'or you do not have permissions to read it.'))
