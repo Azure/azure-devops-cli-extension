81ad1333eb4fdf99dd4d89d78aaa9f532c3cdce0
RakeshMohanMSFT <49954584+RakeshMohanMSFT@users.noreply.github.com>
2022-12-22 16:21:05 +0530

    {azure-devops} Fix #1698: Add example for the 'az repos import create' command
    Fixes https://github.com/Azure/azure-cli-extensions/issues/1698

--- azure-devops/azext_devops/dev/repos/_help.py
+++ azure-devops/azext_devops/dev/repos/_help.py
@@ -41,6 +41,11 @@ def load_repos_help():
     type: group
     short-summary: Manage Git repositories import
     long-summary:
+    examples:
+        - name: The following command imports the public repo fabrikam-open-source to the empty Git repo fabrikam-open-source for the default configuration 
+                az devops configure --defaults organization=https://dev.azure.com/fabrikamprime project="Fabrikam Fiber".
+          text: |-
+              az repos import create --git-source-url https://github.com/fabrikamprime/fabrikam-open-source --repository fabrikam-open-source
     """
 
     helps['repos policy'] = """
